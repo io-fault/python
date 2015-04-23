408b568916cbcca56bd6bc15030cab90fe50891c
fault <critical@fault.io>
2015-04-23 12:57:48 -0700

    Improve palette definition interfaces.

--- terminal/palette.py
+++ terminal/palette.py
@@ -5,6 +5,7 @@ Color code translations for 256-color supporting terminals. Sourced from xterm.
 def gray_palette(index):
 	"""
 	Twenty four shades. Map range(24) for the full palette.
+	Returns a tuple with the 24-bit RGB value and the code.
 
 	White and black are not included.
 	"""
@@ -15,38 +16,44 @@ def gray_palette(index):
 
 	base = index * 10 + 8
 
-	return (base<<16|base<<8|base), index + 232
+	return ((base << 16) | (base << 8) | base), index + 232
 
 def gray_code(color):
 	"""
-	Look up a particular gray code in the gray palette using a 32-bit color.
+	Covert the given 24-bit RGB color into a gray color code.
+
+	If the color does not have an exact match in the palette, the returned code may
+	not be a reaonable substitute for the given color.
 	"""
-	value = value & 0xFF
-	r = (value - 8) // 10
+	r = (color - 8) // 10
 	return r + 232
 
-def color_palette(red, green, blue):
+def color_palette(r, g, b):
 	"""
 	Select a color from the 256-color palette using 0-6 indexes for each color.
 
-	Map the combination of three range(6) instances to render the full palette.
+	Map the product of three range(6) instances to render the full palette.
 	"""
-	code = 16 + (red * 36) + (green * 6) + blue
+	code = 16 + (r * 36) + (g * 6) + b
 
 	red_value = green_value = blue_value = 0
-	if red:
-		red_value = red * 40 + 55
-	if green:
-		green_value = green * 40 + 55
-	if blue:
-		blue_value = blue * 40 + 55
+	if r:
+		red_value = r * 40 + 55
+	if g:
+		green_value = g * 40 + 55
+	if b:
+		blue_value = b * 40 + 55
+
 	value = (red_value << 16) | (green_value << 8) | blue_value
 
 	return (value, code)
 
 def color_code(color):
 	"""
-	Look up a particular gray code in the gray palette using a 32-bit color.
+	Convert the given 24-bit RGB color into a terminal color code.
+
+	If the color does not have an exact match in the palette, the returned code may
+	not be a reaonable substitute for the given color.
 	"""
 	r, g, b = (color & 0xFF0000) >> 16, (color & 0x00FF00) >> 8, (color & 0xFF)
 	ri = (r - 55) // 40
@@ -62,7 +69,27 @@ def color_code(color):
 	code = 16 + (ri * 36) + (gi * 6) + bi
 	return code
 
-grays = dict(map(gray_palette, range(24)))
+def scale_color(r, g, b, initial = 0x5f):
+	"""
+	Map the given RGB color to the one closest in the xterm palette.
+
+	The ranges start at zero, 0x5f, and then increments to 40.
+	"""
+	color = 0
+
+	for x in (r,g,b):
+		color = color << 8
+
+		if x < initial:
+			# initial increment is inconsistent, so handle specially.
+			x = int(round(x / initial)) * initial
+		else:
+			x = initial + (int(round((x - initial) / 40)) * 40)
+
+		# apply value
+		color = color | x
+
+	return color
 
 sixteen_colors = {
 	# grays
@@ -95,3 +122,65 @@ sixteen_colors = {
 	0x008080: 6,
 	0x00FFFF: 14,
 }
+
+def translate(rgb):
+	"""
+	Translate the given RGB color into a terminal color code.
+
+	This function analyzes the color and chooses the appropriate palette according to its
+	value.
+	"""
+	r = (rgb >> 16) & 0xFF
+	g = (rgb >> 8) & 0xFF
+	b = (rgb >> 0) & 0xFF
+
+	# if the values are within a range relative to the average, translate it as a gray
+	avg = ((r + g + b) / 3)
+	for x in (r, g, b):
+		if abs(x - avg) > 16:
+			# component exceeded the distinguishable range.
+			return color_code(scale_color(r, g, b))
+	else:
+		# use scale_gray
+		avg = int(avg)
+		if avg < 0x9:
+			# the grays don't include black and white, so special case them here
+			return color_code(0x0)
+		elif avg > 0xF0:
+			return color_code(0xFFFFFF)
+
+		return gray_code(avg)
+
+	raise RuntimeError("function did not return according to programming")
+
+if __name__ == '__main__':
+	import sys
+	# render a palette to the screen
+	escape = '\x1b['.__add__
+
+	# Part of the initial escape.
+	separator = ';'
+	terminator = 'm'
+
+	fg = '38;5;'
+	bg = '48;5;'
+
+	normal = escape(bg) + '0m'
+
+	args = sys.argv[1:]
+	if args:
+		color, text = args
+		normal = escape('0') + 'm'
+		sys.stderr.write(escape(fg) + color + 'm' + text + normal + ' eof\n')
+		print("color = lambda x: '\\x1b[38;5;{0}m' + x + {1}".format(color, repr(normal)))
+	else:
+		i = 0
+		for r in range(6):
+			for g in range(6):
+				for b in range(6):
+					i = i + 1
+					v, c = color_palette(r, g, b)
+					sys.stderr.write((escape(bg) + str(c) + 'm     ' + normal + ': ' + str(c).rjust(3, ' ') + ' '))
+					if i > 12:
+						sys.stderr.write('\n')
+						i = 0
