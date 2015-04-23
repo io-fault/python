9e1c3d6b7538825a662a4c65bf3b34220c2cc96d
fault <critical@fault.io>
2015-04-23 12:52:42 -0700

    initial stub

---
+++ kernel/security.py
@@ -0,0 +1,10 @@
+"""
+Transformer access to security layers.
+"""
+
+class SIO(object):
+	"""
+	Secure input or output using given @security_state.
+	"""
+	def __init__(self, security_state):
+		pass
