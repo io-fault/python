a84ad9e50867383535bf4c6d6b7aa07f6c83e95a
fault <critical@fault.io>
2015-04-28 10:11:02 -0700

    Move console to IO.

--- terminal/project.py
+++ terminal/project.py
@@ -1,35 +1,30 @@
 """
-Requirements
-=
+[ Requirements ]
 
-Any hidden dependencies or notes about dependencies.
-
-Defense
-=
-
-Defend your project's existence, but only defend modules that are intended for
-external use.
+Mostly built-in Python modules: tty, termios, fcntl.
 """
-abstract = 'terminal display support for character devices and web browsers'
+abstract = 'terminal display support for character devices'
 
-#: Project name.
+# Project name.
 name = 'terminal'
 
-#: The name of the conceptual branch of development.
-fork = 'dark' # Explicit branch name and a codename for the major version of the project.
+# The name of the conceptual branch of development.
+fork = 'darkness' # Explicit branch name and a codename for the major version of the project.
 release = None # A number indicating its position in the releases of a branch. (fork)
 
-#: The particular study or subject that the package is related to.
+# The particular study or subject that the package is related to.
 study = {}
 
-#: Relevant emoji or reference--URL or relative file path--to an image file.
-icon = '💻'
+# Relevant emoji or reference--URL or relative file path--to an image file.
+icon = {
+	'unicode': '💻'
+}
 
-#: IRI based project identity. (project homepage)
-identity = 'https://fault.io/project/python/terminal'
+# IRI based project identity. (project homepage)
+identity = 'https://fault.io/project/terminal/' + fork
 
-#: Responsible Party
+# Responsible Party
 controller = 'fault.io'
 
-#: Contact point for the Responsible Party
+# Contact point for the Responsible Party
 contact = 'mailto:critical@fault.io'
--- terminal/test/test_device.py
+++ terminal/test/test_device.py
@@ -2,8 +2,8 @@ from .. import device
 
 def test_literal_events(test):
 	print(device.literal_events('ff'))
-	test/device.literal_events('f') == (device.Character('literal', 'f', 'f', False, False),)
-	test/device.literal_events('ff') == (device.Character('literal', 'f', 'f', False, False),)*2
+	test/device.literal_events('f') == (device.Character('literal', 'f', 'f', ),)
+	test/device.literal_events('ff') == (device.Character('literal', 'f', 'f',),)*2
 
 if __name__ == '__main__':
 	import sys; from ...development import libtest
