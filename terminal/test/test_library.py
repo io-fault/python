ab681274d11f12fd657f4c5ef50220bacd14aa4f
fault <critical@fault.io>
2015-05-24 11:02:00 -0700

    Add core.Position tests.

--- terminal/test/test_library.py
+++ terminal/test/test_library.py
@@ -1,4 +1,80 @@
 from .. import library
 
-def test_feature(test):
-	test/library.function() == 'value'
+def test_position(test):
+	p = library.Position()
+	test/p.snapshot() == (0,0,0)
+	test/p.get() == 0
+
+	# checking corner cases
+	p.update(2)
+	test/p.snapshot() == (0,2,0)
+
+	# above magnitude
+	test/p.relation() == 1
+
+	test/p.contract(0, 2)
+	test/p.maximum == -2
+	test/p.relation() == 1
+	test/p.get() == 0 # contract offset was at zero
+	test/p.minimum == 0
+
+	p.configure(10, 10, 5)
+	test/p.relation() == 0
+	test/p.get() == 15
+	test/p.maximum == 20
+	test/p.minimum == 10
+
+	p.move(5, -1)
+	test/p.get() == 15
+
+	p.move(4, -1)
+	test/p.get() == 16
+
+	p.move(3, -1)
+	test/p.get() == 17
+
+	p.move(0, -1)
+	test/p.get() == 20
+
+	p.move(0, 1)
+	test/p.get() == 10
+
+	p.move(1, 1)
+	test/p.get() == 11
+
+	p.move(1, 0)
+	test/p.get() == 12
+
+	p.move(1)
+	test/p.get() == 13
+
+	p.update(-1)
+	test/p.get() == 12
+
+	p.update(1)
+	test/p.get() == 13
+
+	p.update(-4)
+	test/p.get() == 9 # before datum
+	test/p.relation() == -1
+
+	p.contract(0, 1)
+	test/p.relation() == -1
+	test/p.get() == 9 # still before datum
+	test/p.offset == -1
+	test/p.magnitude == 9
+	test/p.snapshot() == (10, 9, 19)
+
+	p.contract(-1, -11) # negative contractions expand
+	test/p.snapshot() == (10, 20, 30)
+
+	p.expand(0, 5)
+	test/p.snapshot() == (10, 25, 35)
+
+	# again, but without the change in offset
+	p.expand(35, 5)
+	test/p.snapshot() == (10, 25, 40)
+
+if __name__ == '__main__':
+	import sys; from ...development import libtest
+	libtest.execute(sys.modules[__name__])
