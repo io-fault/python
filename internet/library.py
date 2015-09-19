33bc8eb0dd8877e4bfb73e77601f411f1e54dc63
fault <critical@fault.io>
2015-09-19 00:46:43 -0700

    Add repr to Endpoint and port classification methods.

--- internet/library.py
+++ internet/library.py
@@ -1,5 +1,6 @@
 """
-Endpoint and Service type for referencing connection addresses.
+Endpoint and Service type for referencing internet addresses for
+Internet Protocol version 4 and 6.
 """
 import ipaddress
 
@@ -14,9 +15,19 @@ class Service(int):
 
 		return self >= 0 and self <= 0xFFFF
 
+	def system(self):
+		"Whether or not the Service port is considered a 'well known' port."
+
+		return self >= 0 and self < 1024
+
+	def ephemeral(self):
+		"Whether or not the Service port is considered an 'ephemeral' port by IANA."
+
+		return self >= 49152 and self < 0xFFFF
+
 class Endpoint(tuple):
 	"""
-	Endpoint type, address-port pairs, for internet addresses.
+	Endpoint type for internet addresses.
 	"""
 	__slots__ = ()
 
@@ -36,7 +47,8 @@ class Endpoint(tuple):
 
 	@property
 	def protocol(self, str=str):
-		'Addressing protocol; 4 or 6'
+		"Addressing protocol; 4 or 6"
+
 		return 'ip' + str(self.address.version)
 
 	@property
@@ -55,7 +67,7 @@ class Endpoint(tuple):
 	def __str__(self):
 		"""
 		"<address>:<port>" representation suitable for interpolation into
-		IRI network location.
+		an IRI network location.
 		"""
 
 		if self.interface.version == 6:
@@ -63,13 +75,22 @@ class Endpoint(tuple):
 		else:
 			return str(self.interface) + ':' + str(self.port)
 
+	def __repr__(self):
+		m = __name__
+		n = self.__class__.__name__
+		p = self.protocol
+		i = str(self.interface)
+
+		return "%s.%s.create_%s(%r, %r)" %(m, n, p, i, self.port)
+
 	def __hash__(self):
 		return hash((self.protocol, self.interface, self.port))
 
 	@classmethod
 	def create(Class, interface, port, construct=ipaddress.ip_address):
 		"""
-		Primary method for creating an endpoint.
+		Create an IPv4 or IPv6 endpoint based on the type detected from the string.
+
 		The &ipaddress type will be selected by the &ipaddress.ip_address function.
 		"""
 		return Class((construct(interface), Service(port)))
