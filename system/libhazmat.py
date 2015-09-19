aa3e834b22cf67c1125ae526baca52de8a85a9ad
fault <critical@fault.io>
2015-09-19 10:56:10 -0700

    Implement Segments and use negative codes for distinguishing signal exits.

--- system/libhazmat.py
+++ system/libhazmat.py
@@ -16,6 +16,7 @@ The process management interfaces exist to abstract the operating system's.
 The queue managemnent interfaces provide queue implementations and containment tools for
 interleaving processing chains for cooperative areas of an application.
 """
+
 import os
 import sys
 import collections
@@ -181,8 +182,8 @@ def process_delta(
 		status = getstatus(code)
 		cored = wascore(code) or False
 	elif wassignal(code):
-		event = 'signal'
-		status = - getsig(code) or 0
+		event = 'exit'
+		status = - getsig(code)
 		cored = wascore(code) or False
 	elif wasstopped(code):
 		event = 'stop'
@@ -213,13 +214,16 @@ def chain(iterable, initial, contain = core.contain):
 
 	.. note:: The `initial` container given should not be used again.
 	"""
+
 	param = initial
+
 	for generator in iterable:
 		why = param[0]
 		if param.failed:
 			param = contain(generator.throw, why.__class__, why, why.__traceback__)
 		else:
 			param = contain(generator.send, why)
+
 	return param
 
 class Delivery(object):
@@ -487,7 +491,7 @@ class Memory(bytearray):
 		pagesize = resource.getpagesize()
 		del resource
 	except ImportError:
-		pagesize = 1024
+		pagesize = 1024*4
 
 	__slots__ = ('__weakref__',)
 
@@ -568,3 +572,77 @@ class MemoryContext(object):
 		Explicitly add an object to the available segments.
 		"""
 		self.segments.extend(event)
+
+class Segments(object):
+	"""
+	Iterate over the slices of an active memory map;
+	Weak references of the slices are held to track when
+	its appropriate to close the memory map.
+	"""
+
+	from mmap import mmap as MemoryMap
+	from mmap import ACCESS_READ as ACCESS_MODE
+
+	@classmethod
+	def open(Class, path):
+		f = open(path, 'rb')
+		fd = f.fileno()
+		s = Class(Class.MemoryMap(fd, 0, access=Class.ACCESS_MODE))
+		del f
+		return s
+
+	def __init__(self, memory, start = 0, stop = None, size = 1024*4, memoryview=memoryview):
+		self.range = (start, stop if stop is not None else len(memory), size)
+		self.memory = memory
+		self.weaks = weakref.WeakSet()
+
+	def __del__(self):
+		# The delete method is used as its
+		# the precise functionality that is needed here.
+		#
+		# It is unusual that the weak set will ever be empty
+		# when del is called, but if it is, it's a shortcut.
+
+		if self.weaks is not None:
+			if len(self.weaks) > 0:
+				# Add references
+				self.__iter__ = None
+				self.finals = [
+					weakref.finalize(x, self.decrement) for x in self.weaks
+				]
+				self.count = len(self.finals)
+				self.weaks.clear()
+				self.weaks = None
+			else:
+				# no references to the slices, close memory
+				self.weaks.clear()
+				self.weaks = None
+				self.memory.close()
+		else:
+			# second del attempt, decrement hit zero.
+			self.memory.close()
+
+	def decrement(self):
+		self.count -= 1
+		if self.count == 0:
+			# this should trigger del's second stage.
+			del self.finals[:]
+			del self.finals
+
+	def __iter__(self):
+		view = memoryview(self.memory)
+		i = iter(range(*self.range))
+
+		start = next(i)
+		stop = None
+
+		for stop in i:
+			vslice = view[start:stop]
+			self.weaks.add(vslice)
+			yield vslice
+			start = stop
+		else:
+			# use the last stop to start the final
+			vslice = view[stop:stop+self.range[-1]]
+			self.weaks.add(vslice)
+			yield vslice
