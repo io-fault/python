56d8bd7112ca1845f16f4f3636393d383a40cea2
fault <critical@fault.io>
2015-09-19 15:20:38 -0700

    Reorganize and expand documentation.

--- kernel/abstract.py
+++ kernel/abstract.py
@@ -1,15 +1,66 @@
 """
 Abstract Base Classes and common data structures.
+
+Defines the semantics of the &core classes.
 """
 import abc
+
+class Projection(object, metaclass=abc.ABCMeta):
+	"""
+	Object managing the credentials of an arbitrary entity.
+
+	Used by &Sector instances.
+	"""
+
+	@property
+	@abc.abstractmethod
+	def entity(self):
+		"""
+		The entity that is driving the transactions of the Unit.
+		The conceptual source of the events.
+		"""
+
+	@property
+	@abc.abstractmethod
+	def role(self):
+		"""
+		The role being fulfilled by the entity; usually subaccounts
+		or composite entity positions (salesperson, engineer, etc).
+		"""
+
+	@property
+	@abc.abstractmethod
+	def credentials(self):
+		"""
+		A finite map of systems and their designated credentials.
+		Arbitrary identifiers can be used, but an identifier of the relevant system
+		is usually ideal.
+		"""
+
+	@property
+	@abc.abstractmethod
+	def authorization(self):
+		"""
+		A finite map of authorization tokens acquired.
+		"""
+
+	@property
+	@abc.abstractmethod
+	def transactions(self):
+		"""
+		XXX: Probably remove this or move to Sector.
+
+		A set of transactions being managed by the Unit.
+		Transactions represent active work and the contents
+		of this set may decide whether or not the unit may be reaped.
+		"""
 
 class Resource(metaclass = abc.ABCMeta):
 	@property
 	@abc.abstractmethod
 	def context(self):
 		"""
-		A reference to the &Context that allocated the resource and defines its operating
-		environment.
+		A reference to the &Context and ultimately the &Process supporting the &Resource.
 		"""
 
 	@property
@@ -19,19 +70,125 @@ class Resource(metaclass = abc.ABCMeta):
 		The immediate ascendant referring to the &Resource. There is only one &controller
 		per object and the reference held by the controller is intended to be the critical
 		reference.
+
+		Equivalent to the parent process of a unix system process.
+		"""
+
+class Processor(Resource, metaclass = abc.ABCMeta):
+	"""
+	A resource that performs abstract processing.
+	"""
+
+	@abc.abstractmethod
+	def __init__(self):
+		"""
+		Initialization *should* not require parameters as &requisite is the appropriate
+		interface. Initialization is intended to be little more than object allocation
+		in order to allow pre-allocated pools of Processors to be used.
+		"""
+
+	@abc.abstractmethod
+	def requisite(self, *args, **kw):
+		"""
+		Provide a set of requisites for the &Processor that are required prior
+		to actuation of the Processor.
+
+		Complex &Processors subclasses may require explicit invocation of a class
+		qualified method in order for an instance to be ready for actuation.
 		"""
 
 	@abc.abstractmethod
 	def actuate(self):
 		"""
-		Signal the &Resource that it must be prepared for use after the method call exits.
+		Prepare the &Processor for activity; must be called prior to any &process
+		invocations.
 
-		Can be &None.
+		Processors that are actuated and not terminated nor interrupted are said
+		to be "functioning". A functioning Processor means that the state
+		of the Processor is totally dependent on its implementation and thus
+		subjective.
 		"""
 
-class Transformer(Resource, metaclass=abc.ABCMeta):
+	@abc.abstractmethod
+	def process(self, event, source = None):
+		"""
+		Formal message passing interface for &Processors.
+
+		Ideally, all possible Processor states could be reached using &process.
+		However, this is often not the case as its usually easier to split the
+		functionality across a set of methods.
+		"""
+
+	@abc.abstractmethod
+	def terminate(self, by=None):
+		"""
+		Initiate the termination state of the Processor.
+
+		Termination may not be immediate and is subjected to &Processor specific logic.
+		Some Processors may not support termination at all and invoke an interruption instead.
+
+		The termination process should end in the Processor generating an exit event emitted
+		to the controlling Processor. When an implementation (subclass) does not support
+		termination, it should interrupt and signal exit itself.
+
+		The termination process is revealed by setting the &terminating property to &True
+		upon &terminate being invoked, and then to &False when termination has completed.
+		Additionally, &terminated must be set to &True when termination has completed.
+		"""
+
+	@abc.abstractmethod
+	def interrupt(self, by=None):
+		"""
+		Interrupt the processor prohibiting further state changes.
+
+		Interrupts are primarily used in cases of exceptions and timeouts.
+		It attempts to provide a means to manage Processor exits regardless of
+		implementation state--being the difference between termination and interruption.
+
+		Unlike termination, interrupts are immediate, and there is *no* "interrupting" state.
+
+		Interrupting a Processor does *not* trigger exit events to be sent to the controlling
+		Processor; rather it is up to the interruptor to determine how to proceed as the
+		interrupt may have been inherited from a controlling &Sector; the Sector is exiting,
+		not the descendant Processors.
+		"""
+
+	@abc.abstractmethod
+	def atexit(self, callback):
+		"""
+		Register the callback to be executed when the &Processor exits.
+
+		The callback will be executed when the &Processor has been detached from
+		the &Resource.controller.
+
+		If the &Processor is interrupted by a controlling &Sector, the exit handler
+		will never be called.
+
+		The &callback must be a callable that can accept a single parameter, the
+		&Processor that exited.
+		"""
+
+	@property
+	@abc.abstractmethod
+	def product(self):
+		"""
+		An attribute holding the product of a &Processor.
+		Essentially, a return object, but not always used.
+		"""
+
+	@property
+	@abc.abstractmethod
+	def exceptions(self):
+		"""
+		The set of exceptions associated with the &Processor.
+
+		Used to track how a fault occurred.
+		"""
+
+class Transformer(Processor, metaclass=abc.ABCMeta):
 	"""
-	An object that manages a transformation and its dependent state.
+	A &Processor whose termination process depends on the completion
+	of the preceeding Transformer.
 	"""
 
 	@property
@@ -44,22 +201,22 @@ class Transformer(Resource, metaclass=abc.ABCMeta):
 		return False
 
 	@abc.abstractmethod
-	def process(self, sequence):
+	def drain(self):
 		"""
-		Instruct the transformer to process the @sequence immediately.
-		"""
-		pass
+		Initiate a drain operation on the Transformer, if the &Transformer
+		returns a not &None object, it is used to register a callback
+		that is executed on completion. If &None is returned, it indicates
+		that the drain operation was successfully completed.
 
-	@property
+		Drain operations are not entirely consistent with flushes in that
+		they are not mere buffers.
+		"""
+
 	@abc.abstractmethod
 	def emit(self, event):
 		"""
-		Method used by @process to send the sequence to the linked transformer.
-
-		Given that no connection is made and the Transformer is not a terminal,
-		the method should raise an exception to denote the failure.
+		Property set by &Flow Processors to send an effect of the &process method downstream.
 		"""
-		pass
 
 	@abc.abstractmethod
 	def emission(self, event):
@@ -73,77 +230,41 @@ class Transformer(Resource, metaclass=abc.ABCMeta):
 		of emit can vary.
 		"""
 
-class Processor(Resource, metaclass = abc.ABCMeta):
-	"""
-	A resource that performs abstract processing.
-	"""
-
-	@abc.abstractmethod
-	def terminate(self, by=None):
-		"""
-		Cause the termination of the Processor.
-		Termination may not be immediate and is subjected to &Processor specific logic.
-		"""
-
-	@abc.abstractmethod
-	def interrupt(self, by):
-		"""
-		Interrupt the processor terminating work.
-
-		Interrupt is used exclusively by external Resources. Usually used for administrative
-		interrupts.
-		"""
-
-	@abc.abstractmethod
-	def atexit(self, callback):
-		"""
-		Register the callback to be executed when the &Processor exits.
-
-		The callback will be executed when the &Processor has been detached from
-		the &Resource.controller.
-		"""
-
-	@property
-	@abc.abstractmethod
-	def product(self):
-		"""
-		An attribute holding the product of a &Processor. Essentially, a return object, but
-		normally unused.
-		"""
-
-	@property
-	@abc.abstractmethod
-	def exception(self):
-		"""
-		The exception attributed to the exit of the &Processor.
-		"""
-
-	@property
-	@abc.abstractmethod
-	def cascades(self):
-		"""
-		Whether or not the failure is inherited by the &Resource.controller.
-
-		Defaults to &False for &Sector classes.
-		"""
-
 class Sector(Processor, metaclass = abc.ABCMeta):
 	"""
 	A group of &Processor instances. The set of Processors are organized
 	by their type, but have no name. Sector subclasses can be made in
-	order to provide access to the subprocessors.
-	"""
+	order to provide named access to the subprocessors.
 
-class Unit(Processor, metaclass = abc.ABCMeta):
-	"""
-	A logical process used contain a set of units.
+	Sectors are analogous to unix system processes.
 	"""
 
 	@property
 	@abc.abstractmethod
-	def identifier(self):
+	def projection(self) -> Projection:
 		"""
-		The name of the Program. Used to identify a program from the logical process.
+		Defaults to &None, but provides access to any credentials of the user or group
+		that is being represented by the &Sector.
+		"""
+
+	@property
+	@abc.abstractmethod
+	def scheduler(self):
+		"""
+		A &Processor controlled by the &Sector that provides scheduling
+		support for controlled &Processor instances referenced by the &Sector.
+		"""
+
+class Unit(Processor, metaclass = abc.ABCMeta):
+	"""
+	The set of root &Processor instances that make up the Unit.
+	"""
+
+	@property
+	@abc.abstractmethod
+	def identity(self):
+		"""
+		The name of the Unit. Used by the &Process to distinguish &Unit instances.
 		"""
 
 	@property
@@ -154,14 +275,16 @@ class Unit(Processor, metaclass = abc.ABCMeta):
 
 		During initialization, the items of this sequence are called and given
 		the &Program instance as its sole parameter.
+
+		Exceptions from roots will raise a Panic.
 		"""
 
 	@property
 	@abc.abstractmethod
-	def structure(self):
+	def hierarchy(self):
 		"""
 		A dictionary tree whose leaves are initializors for the corresponding index
-		entries that maintain the &Program's state.
+		entries that maintain the &Unit's state.
 		"""
 
 	@property
@@ -212,51 +335,5 @@ class Flow(Processor, metaclass=abc.ABCMeta):
 		Clear the cited &obstruction.
 
 		If there are no remaining obstructions, signal any callbacks registered with &watch.
-		"""
-
-class Projection(object, metaclass=abc.ABCMeta):
-	"""
-	Object managing the credentials of an arbitrary entity.
-	"""
-
-	@property
-	@abc.abstractmethod
-	def entity(self):
-		"""
-		The entity that is driving the transactions of the Unit.
-		The conceptual source of the events.
-		"""
-
-	@property
-	@abc.abstractmethod
-	def role(self):
-		"""
-		The role being fulfilled by the entity; usually subaccounts
-		or composite entity positions (salesperson, engineer, etc).
-		"""
-
-	@property
-	@abc.abstractmethod
-	def credentials(self):
-		"""
-		A finite map of systems and their designated credentials.
-		Arbitrary identifiers can be used, but an identifier of the relevant system
-		is usually ideal.
-		"""
-
-	@property
-	@abc.abstractmethod
-	def authorization(self):
-		"""
-		A finite map of authorization tokens acquired.
-		"""
-
-	@property
-	@abc.abstractmethod
-	def transactions(self):
-		"""
-		A set of transactions being managed by the Unit.
-		Transactions represent active work and the contents
-		of this set may decide whether or not the unit may be reaped.
 		"""
 
