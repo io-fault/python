6d116b60b5adce07e051813346cb38d5bddfa15d
fault <critical@fault.io>
2015-09-20 14:56:02 -0700

    Comments.

--- kernel/bin/faultd.py
+++ kernel/bin/faultd.py
@@ -1,17 +1,24 @@
 """
 Execute the fault (root) daemon for managing a set of services.
+
+By default, this script resolves the root service from the &FAULTD_DIRECTORY
+environment variable. This can be avoided by importing the module,
+and using &initialize as a root for a Unit.
 """
 
 import sys
 import os
 
 def initialize(unit):
+	# Avoid import as it exec() if it's not the faultd hardlink.
 	from .. import libroot
 	from .. import libservice
 	from .. import library
 
 	# init listening interfaces
 	library.core.Ports.load(unit)
+
+	# No command line options atm. Maybe an override for the faultd directory.
 	command_params = unit.context.process.invocation.parameters
 
 	# Root Daemon Control
@@ -25,11 +32,14 @@ if __name__ == '__main__':
 	# Some redundancy here for supporting faultd hardlinks.
 
 	if os.environ.get('FAULTD') is None:
+		# Initial Python Invocation
+		# Resolve the hardlink and exec().
 		params = sys.argv[1:]
 		os.environ['FAULTD'] = str(os.getpid())
 		py = os.environ['PYTHON'] = sys.executable
 
 		from .. import libservice
+
 		r = libservice.identify_route()
 		rs = libservice.Service(r, 'root')
 		if not rs.exists():
