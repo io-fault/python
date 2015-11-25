914a4b23eef48824bea464e544b3c29556d123ef
fault <critical@fault.io>
2015-11-25 15:29:51 -0700

    Further documentation updates supporting eclectic.

--- time/__init__.py
+++ time/__init__.py
@@ -5,26 +5,28 @@
 ! WARNING:
 	Chronometry is a work in progress.
 
-chronometry is a date and time package based on the built-in Python `int`.
+chronometry is a date and time package based on the built-in Python &int.
 By default, timestamps have nanosecond precision granting common applications
 more than enough detail about a point in time. For specialized purposes, the
-units handled by chronometry can be arbitrarily extended--the `int` subclasses
+units handled by chronometry can be arbitrarily extended--the &int subclasses
 represent a designated unit and a common Time Context, group of unit classes,
 provides the necessary linkage and transformations for conversion across
 classes.
 
 Calendar Support:
 
- * Proleptic Gregorian
+	- Proleptic Gregorian
 
 chronometry's APIs are *not* compatible with the standard library's datetime module.
 
-The surface functionality is provided library @:package.library:
+&.library will be referred to as `libtime` throughout the examples in this documentation.
+
+The surface functionality is provided &.library:
 
 #!/pl/python
 	import chronometry.library
 
-Current date and time as a @:package.library.Timestamp.
+Current date and time as a &.library.Timestamp.
 
 #!/pl/python
 	now = chronometry.library.now() # UTC
@@ -46,20 +48,21 @@ However, the above actually represents.
 Usually, using the `date` keyword is best way to to work with
 literal dates.
 
+#!/pl/python
 	assert chronometry.library.Date.of(date=(1982,5,18)) == date
 
 The calendrical **representation** only takes effect through certain
 interfaces.
 
 #!/pl/python
-	ts = chronometry.library.Timestamp.of(iso="2001-01-01T05:30:01")
+	ts = libtime.Timestamp.of(iso="2001-01-01T05:30:01")
 	print(repr(ts))
-	chronometry.library.Timestamp.of(iso='2001-01-01T05:30:01.000000')
+	libtime.Timestamp.of(iso='2001-01-01T05:30:01.000000')
 
 And from a datetime tuple.
 
 #!/pl/python
-	ts2 = chronometry.library.Timestamp.of(datetime = (2001, 1, 1, 5, 30, 1, 0))
+	ts2 = libtime.Timestamp.of(datetime = (2001, 1, 1, 5, 30, 1, 0))
 	assert ts == ts2
 
 chronometry PiTs do not perform calendrical validation; rather, fields with excess
@@ -68,7 +71,7 @@ overflow. For chronometry, this choice is deliberate and the user is expected to
 perform any desired validation.
 
 #!/pl/python
-	pit = chronometry.library.Date.of(date=(1982,5,0))
+	pit = libtime.Date.of(date=(1982,5,0))
 
 The assigned `pit` now points to the last day of the month preceding the fifth
 month in the year 1982. This kind of wrapping allows chronometry users to *quickly*
@@ -81,7 +84,7 @@ chronometry can easily answer questions like, "What was third weekend of the fif
 month of last year?".
 
 #!/pl/python
-	pit = chronometry.library.now()
+	pit = libtime.now()
 	pit = pit.update('day', 0, 'month') # set to the first day to avoid overflow
 	pit = pit.rollback(year=1) # subtract one gregorian year
 	pit = pit.update('month', 5-1, 'year') # set to the fifth month
@@ -93,7 +96,7 @@ last weekend of the month?". It's not a problem::
 
 #!/pl/python
 	# move the beginning of month (to avoid possible day overflow)
-	pit = chronometry.library.now().update('day', 0, 'month')
+	pit = libtime.now().update('day', 0, 'month')
 	# to the next month and then to the end of the previous
 	pit = pit.elapse(month = 1).update('day', -1, 'month') # move to the end of the month.
 	# 0 is the beginning of the week, so -1 is the end of the prior week.
@@ -103,9 +106,9 @@ On day overflow, the following illustrates the effect::
 
 #!/pl/python
 	# working with a leap year
-	pit = chronometry.library.Timestamp.of(iso='2012-01-31T18:55:33.946259')
+	pit = libtime.Timestamp.of(iso='2012-01-31T18:55:33.946259')
 	pit.elapse(month=1)
-	chronometry.library.Timestamp.of(iso='2012-03-02T18:55:33.946259')
+	libtime.Timestamp.of(iso='2012-03-02T18:55:33.946259')
 
 Month arithmetic does not lose days in order to align the edge of a month.
 In order to keep overflow from causing invalid calculations, adjust to the
@@ -116,7 +119,7 @@ Things can get even more interesting when asking,
 alignment in order to be answered::
 
 #!/pl/python
-	pit = chronometry.library.now()
+	pit = libtime.now()
 	pit = pit.update('day', 0, 'month') # let's say this month
 	# but we need the end of the month
 	pit = pit.elapse(month=1)
@@ -146,7 +149,7 @@ Measuring the execution time of a code block is easy with a chronometry stopwatc
 	def work():
 		pass
 
-	with chronometry.library.clock.stopwatch() as snapshot:
+	with libtime.clock.stopwatch() as snapshot:
 		work()
 
 	print(snapshot())
@@ -165,13 +168,13 @@ However, once the block exits, the stopwatch will stop tracking elapsed time.
 Deltas and meters provide a means to track the change in, monotonic, time:
 
 #!/pl/python
-	for measure_since_last_iteration in chronometry.library.clock.delta():
+	for measure_since_last_iteration in libtime.clock.delta():
 		pass
 
 Meters are like deltas, but provide the *total* measurement:
 
 #!/pl/python
-	for measure_since_first_iteration in chronometry.library.clock.meter():
+	for measure_since_first_iteration in libtime.clock.meter():
 		pass
 
 [ Time Zones ]
@@ -180,8 +183,8 @@ Meters are like deltas, but provide the *total* measurement:
 Time zone adjustments are supported by zone objects:
 
 #!/pl/python
-	pit = chronometry.library.now()
-	tz = chronometry.library.zone('America/Los_Angeles')
+	pit = libtime.now()
+	tz = libtime.zone('America/Los_Angeles')
 	local_pit = tz.localize(pit)
 	print(local_pit.select('iso'))
 """
