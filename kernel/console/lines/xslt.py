96aa196af152e47a2d3e4b3c643a63ae967373d2
fault <critical@fault.io>
2015-12-16 19:55:51 -0700

    Add xslt and xml lines.

---
+++ kernel/console/lines/xslt.py
@@ -0,0 +1,23 @@
+"""
+XSLT language profile.
+"""
+import itertools
+from .. import libfields
+
+indentation_width = 1
+
+_keywords = [
+	'apply-templates',
+	'sort',
+	'attribute',
+	'element',
+]
+keywords = {y: y for y in map(libfields.String, _keywords)}
+cores = {}
+
+terminators = {y: y for y in map(libfields.Constant, "<>")}
+separators = {}
+routers = {':': libfields.Constant(":")}
+operators = {y: y for y in map(libfields.Constant, "@!&^*%+=-|\\/<>?~")}
+groupings = {y: y for y in map(libfields.Constant, "()[]{}")}
+quotations = {y: y for y in map(libfields.Constant, ("'", '"',))}
