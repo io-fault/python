8248995f6cfc73546b1cbf37704cf11bd4ef0997
fault <critical@fault.io>
2016-01-06 16:48:49 -0700

    Add system interface and explain how security is optionally loaded.

--- kernel/security.py
+++ kernel/security.py
@@ -1,5 +1,9 @@
 """
 &.library.Transports support for PKI based security layer.
+
+Much of the libio security functionality is structured around conditionally
+importing &.security in order to avoid the memory and processing
+overhead involved.
 """
 
 from ..cryptography import library as libcrypt
@@ -34,3 +38,10 @@ def public(certificates=()):
 		_public_context = libcrypt.pki.Context(certificates=certificates+())
 
 	return _public_context
+
+def system(certificates=()):
+	"""
+	Initialize and return the system's TLS security context.
+	Operating system dependant; some platforms may refer to &public.
+	"""
+	raise NotImplementedError
