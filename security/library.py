a855eeeee5d0f487e8849eb25032a34a4bd1f6f8
fault <critical@fault.io>
2016-01-18 09:51:26 -0700

    Initial work on implementation requirements and generalization of interfaces.
    (crypto doesn't want to buy into a single implementation)

--- security/library.py
+++ security/library.py
@@ -1,8 +1,26 @@
 """
-Primary Access.
+Public APIs.
+
+Provides access to the selected SSL implementation, the default public certificate set,
+the host system's certificate set.
 """
+import functools
+import os
+
+implementation_requirement = os.environ.get('SSL_IMPLEMENTATION', 'OpenSSL')
+
 try:
-	from . import openssl
+	from . import openssl as pki
 	implementation = 'OpenSSL'
 except ImportError:
 	raise ImportError("no available security implementation")
+
+if implementation_requirement != implementation:
+	raise ImportError("&/unix/env/SSL_IMPLEMENTATION requirement could not be met")
+
+@functools.lru_cache(2)
+def context(*modules):
+	"""
+	Return a Context instance for the requested set.
+	"""
+	return pki.Context(certificates=[x.bytes() for x in modules])
