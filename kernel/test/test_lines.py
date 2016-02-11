2dec148651265899819947f7a8450b6964da721b
fault <critical@fault.io>
2016-02-11 10:32:46 -0700

    Moved to console.

--- kernel/test/test_lines.py
+++
@@ -1,8 +0,0 @@
-from ..console import lines
-
-def test_python(test):
-	lines.profile('python')
-
-if __name__ == '__main__':
-	import sys; from ...development import libtest
-	libtest.execute(sys.modules['__main__'])
