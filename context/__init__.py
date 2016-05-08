da0dbc296a25ac5c06b8bc207b214c4c57c5ef0e
fault <critical@fault.io>
2016-05-08 13:40:19 -0700

    Remove extension loading function from context.

--- context/__init__.py
+++ context/__init__.py
@@ -4,7 +4,4 @@ Context Package Representation
 __type__ = 'context-representation'
 __pkg_bottom__ = True
 
-from ..development import extension
-import_extension_module = extension.load
-del extension
 context = None
