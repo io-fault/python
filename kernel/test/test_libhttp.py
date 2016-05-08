09f382ba58d8813247e6454d2d34cb9ceebe743b
fault <critical@fault.io>
2016-05-08 16:46:29 -0700

    Update tests for apparently new types.

--- kernel/test/test_libhttp.py
+++ kernel/test/test_libhttp.py
@@ -9,26 +9,26 @@ def test_adapt(test):
 	mr = library.libmedia.Range.from_string("text/plain, */*")
 
 	output = library.adapt(None, mr, "Text to be encoded")
-	test/output == (plain, (b'Text to be encoded',))
+	test/output == (plain, b'Text to be encoded')
 
 	mr = library.libmedia.Range.from_string("application/json, */*")
 	input = {'some': 'dictionary'}
 	expect = (json.dumps(input).encode('utf-8'),)
 	output = library.adapt(None, mr, input)
 	test/output[0] == jsont
-	test/output[1] == expect
+	test/output[1] == expect[0]
 
 	mr = library.libmedia.Range.from_string("application/json, */*")
 	input = ['some', 'list', 'of', 1, 2, 3]
 	expect = (json.dumps(input).encode('utf-8'),)
 	output = library.adapt(None, mr, input)
-	test/output == (jsont, expect)
+	test/output == (jsont, expect[0])
 
 	mr = library.libmedia.Range.from_string("application/octet-stream")
 	input = b'datas'
 	expect = (input,)
 	output = library.adapt(None, mr, input)
-	test/output == (octets, expect)
+	test/output == (octets, expect[0])
 
 if __name__ == '__main__':
 	import sys; from ...development import libtest
