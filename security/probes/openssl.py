d2981040351c149ce065eec8d1d8129662e58ada
fault <critical@fault.io>
2016-05-25 23:27:08 -0700

    Update openssl probe and emit proper library sets.

--- security/probes/openssl.py
+++ security/probes/openssl.py
@@ -2,19 +2,14 @@ from ...development import libfactor
 from ...development import libprobe
 from ...routes import library as libroutes
 
+_extract_nids=r"""cat {} | grep 'define[\t ]*NID' | sed 's/#[\t ]*define[ 	]*NID_/OPENSSL_NID(/;s/[ 	]*[0-9]*$/) \\/'"""
+
 parameters = {
 	'executable':
 		"The openssl executable used to identify headers and library directories.",
 }
 
 libfactor.load('system.probe')
-
-import sys
-import os.path
-import shell_command
-import shutil
-
-extract_nids=r"""cat {} | grep 'define[\t ]*NID' | sed 's/#[\t ]*define[ 	]*NID_/OPENSSL_NID(/;s/[ 	]*[0-9]*$/) \\/'"""
 
 def locate_openssl_object_header(executable = 'openssl'):
 	"""
@@ -26,11 +21,20 @@ def locate_openssl_object_header(executable = 'openssl'):
 	objh = headers / 'openssl' / 'objects.h'
 	return headers, prefix / 'lib', objh
 
-def deploy(probe, module, role):
-	headers, libdir, objh = locate_openssl_object_header()
-	nid_refs = shell_command.shell_output(extract_nids, str(objh))
+data = None
 
-	return {
+def report(probe, role, module):
+	global data
+	return data
+
+def deploy(probe, role, module):
+	import shell_command
+
+	global data, _extract_nids
+	headers, libdir, objh = locate_openssl_object_header()
+	nid_refs = shell_command.shell_output(_extract_nids, str(objh))
+
+	data = {
 		'preprocessor.defines': [
 			("OSSL_NIDS", nid_refs + "\n"),
 		],
@@ -40,7 +44,7 @@ def deploy(probe, module, role):
 		'system.include.directories': set([
 			headers,
 		]),
-		'libraries': (
-			'ssl',
+		'system.library.set': (
+			'ssl', 'crypto', 'z',
 		)
 	}
