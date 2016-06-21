77dd16d4e93e75580c82bdac318fdabb8962670d
fault <critical@fault.io>
2016-06-21 11:25:55 -0700

    Use factor_type name for proper qualification and remove pkg_bottom.

--- system/__init__.py
+++ system/__init__.py
@@ -1,6 +1,4 @@
 """
 fault.system is a project that provides process management for Python applications.
 """
-
-__pkg_bottom__ = True # Use this to detect the root package module of a project.
-__type__ = 'project'
+__factor_type__ = 'project'
