6bd40632e6eaca633617050d5dd96290bad05a8c
fault <critical@fault.io>
2016-06-22 16:25:16 -0700

    Use new factor type protocol.

--- context/root.py
+++ context/root.py
@@ -1,9 +1,8 @@
 """
 fault context package containing core Python projects.
 """
-__type__ = 'context'
-__pkg_bottom__ = True # Use this to detect the root package module of a project.
-__canonical__ = __pkg_cname__ = 'fault' # canonical package name
+__factor_type__ = 'context'
+__canonical__ = 'fault' # canonical package name
 
 try:
 	from . import context
