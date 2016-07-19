4c9b1dbb06565405b23bc482d7b47fa625dae636
fault <critical@fault.io>
2016-07-19 16:36:55 -0700

    Add a simple coverage test.

--- text/test/test_library.py
+++ text/test/test_library.py
@@ -30,6 +30,77 @@ def test_Parser_emphasis(test):
 		('emphasis', 'thrice', 1), ('text', '!')]
 	test/list(function("Some *emphasis!*, not **twice**, but *thrice*!")) == expect
 
+txt = """
+[Section 1]
+
+Paragraph 1.
+
+	- SetItem1
+	- SetItem2
+	- &Reference
+
+/DictItem1
+	DictValue1
+/DictItem2
+	DictValue2
+/&Reference
+	DictValue3
+
+Implicit Title:
+	# SequenceItem1
+	# SequenceItem2
+	# SequenceItem3
+	# &Reference
+
+#!/pl/python
+	Typed Character Matrix
+
+#!/pl/python
+	Subsequent Typed Character Matrix
+
+Paragraph 3. `Inline Literal`. (With-Cast)`Inline Literal`.
+
+Emphasis Types:
+	# *Plain*
+	# **Strong**
+	# ***Excessive***
+
+References:
+	# &<URL>
+	# &[Section 2]
+	# &NoTerminator
+
+! WARNING:
+	Admonition
+
+! ERROR:
+	Admonition
+	#!/pl/python
+		AdmonitionCharacterMatrix
+
+[Section 1 >> Subsection 1]
+
+Subsection 1.1 Paragraph 1.
+
+Paragraph 2. &[Section 1]
+
+[Section 2]
+
+Paragraph 1.
+
+Paragraph 2.
+"""
+
+def test_document(test):
+	"""
+	Construct an XML document from a fault.text document and validate its structure.
+
+	! WARNING:
+		Does not perform any checks against the XML.
+	"""
+	xmli = library.XML.transform('txt:', txt, encoding='utf-8')
+	xml = b''.join(xmli)
+
 if __name__ == '__main__':
 	import sys
 	from ...development import libtest
