f92c3f46ffb99acc23611ae38235effbdc6bdb67
fault <critical@fault.io>
2016-07-19 16:45:10 -0700

    Describe executables.

--- kernel/__init__.py
+++ kernel/__init__.py
@@ -1,4 +1,15 @@
 """
 Asyncrhonous I/O framework supporting HTTP, DNS, and SMTP.
+
+[ Executables ]
+/&.bin.control
+	Control a faultd instance and manage its configuration.
+/&.bin.service
+	Manage the configuration of a faultd instance that is not running.
+/&.bin.rootd
+	Start faultd.
+/&.bin.cache
+	Fetch an HTTP resource and store it in the current working directory using the
+	resource's name.
 """
 __factor_type__ = 'project'
