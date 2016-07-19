9641ad75c9091b45eeaba0c65e30b3041cca84c7
fault <critical@fault.io>
2016-07-19 16:44:53 -0700

    Describe executables.

--- text/__init__.py
+++ text/__init__.py
@@ -1,3 +1,12 @@
 """
+[ Executables ]
+
+/&.bin.xml
+	Write structured XML to standard output using the fault.text from standard input.
+	Most errors that occur will be reported using XML elements describing the failure.
+
+/&.bin.tokenize
+	Emit the tokens generated from the fault.text given to standard input to standard
+	output. Generally only used by developers working on &..text.
 """
 __factor_type__ = 'project'
