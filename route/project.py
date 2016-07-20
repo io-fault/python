cb88c1802412414b1fc66c17848dda154acc2234
fault <critical@fault.io>
2016-07-20 09:38:05 -0700

    Update project information for consistency

--- route/project.py
+++ route/project.py
@@ -1,22 +1,9 @@
-#: Project name.
+identity = 'https://fault.io/project/python/fault.routes'
 name = 'routes'
-abstract = 'filesystem path and module path manipulation tools'
+abstract = 'Filesystem and module path manipulation tools'
 icon = '🚏'
 
-#: IRI based project identity. (project homepage)
-identity = 'http://fault.io/case/python/routes'
-
-#: Responsible Party
-meaculpa = 'fault.io'
-
-#: Contact point for the Responsible Party
-contact = 'mailto:critical@fault.io'
-
-#: Conceptual Branch
 fork = 'deadend'
 
-#: Version tuple: (major, minor, patch)
-version_info = (0, 1, 0)
-
-#: The version string.
-version = '.'.join(map(str, version_info))
+controller = 'fault.io'
+contact = 'mailto:critical@fault.io'
