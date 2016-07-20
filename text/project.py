e813f3167830d0747a28076abb14807b57a74a6c
fault <critical@fault.io>
2016-07-20 12:41:40 -0700

    Imply period.

--- text/project.py
+++ text/project.py
@@ -1,6 +1,6 @@
 identity = 'https://fault.io/project/python/fault.text'
 name = 'text'
-abstract = 'Text Formatting Language for developer documentation and structured status.'
+abstract = 'Text Formatting Language for developer documentation and structured status'
 icon = '📜'
 
 fork = 'void'
