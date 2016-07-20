d82c634c8f1af10af6b3d1760414680e187ed905
fault <critical@fault.io>
2016-07-20 09:38:05 -0700

    Update project information for consistency

--- internet/project.py
+++ internet/project.py
@@ -1,21 +1,12 @@
-abstract = 'internet protocol implementation project'
-
+identity = 'https://fault.io/project/python/fault.internet'
 name = 'internet'
-fork = 'darpa'
-
+abstract = 'Internet protocol implementations (HTTP and SMTP) for Python'
 icon = '📡'
 
-#: IRI based project identity. (project homepage)
-identity = 'https://fault.io/project/python/internet'
+fork = 'darpa'
 
-#: Responsible Party
 controller = 'fault.io'
-
-#: Contact point for the Responsible Party
 contact = 'mailto:critical@fault.io'
 
-#: Version tuple: (major, minor, patch)
-version_info = (0, 1, 0)
-
-#: The version string.
+version_info = (0, 0, 0)
 version = '.'.join(map(str, version_info))
