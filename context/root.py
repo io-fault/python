3d29954facc0587d7eb3fee19c0c80f8fa4e1a9d
fault <critical@fault.io>
2016-07-19 20:10:12 -0700

    Describe some of the projects at a more conceptual level.

--- context/root.py
+++ context/root.py
@@ -1,5 +1,24 @@
 """
-fault context package containing core Python projects.
+Python foundation projects.
+
+[Development Tools]
+
+The &..development package provides tools for managing construction of builds, and
+&..console.bin.editor provides a source code editor for terminals.
+
+[Application and Server Framework]
+
+The &..io package provides a framework for asynchronous applications. It is essentially an
+actor model implementation for event driven architectures.
+
+[Text Formatting Language]
+
+The development tools and &..factors provide a means for publishing releases with
+documentation. A form of structured text called `fault.text` is implemented by &..text
+and used by &..factors to process and structure documentation strings and comments.
+
+A custom language is used for consistent integration with &..factors and an XML
+focused solution for easier web publishing.
 """
 __factor_type__ = 'context'
 __canonical__ = 'fault' # canonical package name
