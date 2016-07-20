b923e528beeffff0c64866c19dce7344caf25905
fault <critical@fault.io>
2016-07-20 09:38:05 -0700

    Update project information for consistency

--- web/project.py
+++ web/project.py
@@ -1,12 +1,9 @@
-name = 'web'
-fork = 'funnel'
 identity = 'https://fault.io/project/python/web'
+name = 'web'
 abstract = 'process management tools for web interfaces'
 icon = '🕸'
-release = None
+
+fork = 'funnel'
 
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'
-
-version_info = (0, 1, 0)
-version = '.'.join(map(str, version_info))
