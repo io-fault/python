59307bd9aa1d52e9c5e687cfbd08ef7cdf434d5e
fault <critical@fault.io>
2016-07-20 09:38:05 -0700

    Update project information for consistency

--- context/project.py
+++ context/project.py
@@ -1,9 +1,12 @@
-abstract = 'fault.io context package'
-identity = 'https://fault.io/project/python/context'
+identity = 'https://fault.io/project/python/fault.context'
 name = 'context'
-fork = 'shade'
-icon = '𝑓(𝓍)'
+abstract = 'fault.io context package'
+icon = '<https://fault.io>'
+
+fork = 'foundation'
+
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'
-version_info = (0, 1, 0)
+
+version_info = (0, 0, 0)
 version = '.'.join(map(str, version_info))
