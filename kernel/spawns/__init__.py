8c9c17d4f23213c30012123c91088f4b6b85a1a7
fault <critical@fault.io>
2016-07-30 08:59:56 -0700

    Initial commit of spawns schema for validating invocation.xml files.

---
+++ kernel/spawns/__init__.py
@@ -0,0 +1,12 @@
+"""
+Schema and tools for &.bin.rootd invocation files.
+
+The fault daemon's configuration directory is a set of directories
+containing invocation files. This schema describes its format.
+"""
+namespace = 'https://fault.io/xml/spawns'
+
+from ...xml import libfactor
+libfactor.load('schema')
+
+__factor_type__ = 'xml.executable'
