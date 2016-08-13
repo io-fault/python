ccb84c687d233e530e3b2d9e67e01329d48936e7
fault <critical@fault.io>
2016-08-13 07:29:57 -0700

    Add status field.

--- security/project.py
+++ security/project.py
@@ -4,6 +4,7 @@ abstract = 'Interfaces to ciphers, protocols, and cryptography related concepts'
 icon = '🔑'
 
 fork = 'shade'
+status = 'flux'
 
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'
