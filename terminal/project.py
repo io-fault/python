c29ce6d8b9fe164595d25032725092f8ead6ba9c
fault <critical@fault.io>
2016-08-16 13:20:43 -0700

    Add status field and improve description sentence.

--- terminal/project.py
+++ terminal/project.py
@@ -1,9 +1,10 @@
 identity = 'https://fault.io/project/python/fault.terminal'
 name = 'terminal'
-abstract = 'Terminal display support for character devices'
+abstract = 'Low-level toolkit for controlling terminal displays'
 icon = '🖥'
 
 fork = 'darkness'
+status = 'flux'
 
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'
