941eb38d1d2fbf9d1565173977af2cb7a5327480
fault <critical@fault.io>
2016-08-18 19:33:34 -0700

    Update.

--- internet/project.py
+++ internet/project.py
@@ -1,13 +1,11 @@
-identity = 'https://fault.io/project/python/fault.internet'
+identity = 'http://fault.io/python/fault.internet'
 name = 'internet'
 abstract = 'Internet protocol implementations (HTTP, DNS, and SMTP) for Python'
 icon = '📡'
 
+versioning = 'continuous'
 fork = 'darpa'
 status = 'flux'
 
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'
-
-version_info = (0, 0, 0)
-version = '.'.join(map(str, version_info))
