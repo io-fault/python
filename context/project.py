dc7dad31758fdb22456d88a62824981fefd619a9
fault <critical@fault.io>
2016-09-08 10:56:11 -0700

    Use unqualified names

--- context/project.py
+++ context/project.py
@@ -1,13 +1,11 @@
-identity = 'http://fault.io/python/fault.context'
+identity = 'http://fault.io/python/context'
 name = 'context'
 abstract = 'Context package for the fault.io core Python projects'
 icon = '<http://fault.io>'
 
 fork = 'foundation'
+versioning = 'continuous'
 status = 'flux'
 
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'
-
-version_info = (0, 0, 0)
-version = '.'.join(map(str, version_info))
