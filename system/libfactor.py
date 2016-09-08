4d3abd693228c31c3c037e0c522bff0ba3af2356
fault <critical@fault.io>
2016-09-07 23:26:57 -0700

    Remove libconstruct..libfpi specific parts.
    The introduction of `induct` allows system.libfactor to enjoy greater
    ignorance of the build process.

--- system/libfactor.py
+++ system/libfactor.py
@@ -6,109 +6,15 @@ selected in order to get the appropriate output to use with the program.
 &.system.libfactor provides the necessary configuration and functions for identifying
 the appropriate files to use at runtime.
 
-&.system.libfactor is canonical; &.development.libconstruct uses it to identify where
+&.system.libfactor is canonical; &.development.libfpi uses it to identify where
 targets should be processed and how to access them for implementation or installation.
-
-[ Properties ]
-
-/selected_context
-	Construction context to load from.
-
-/selected_role
-	Construction role to load from.
-
-/selections
-	A mapping providing the selected role to use for the factor module.
-
-/python_triplet
-	The `-` separated strings representing the currently executing Python context.
-	Used to construct directories for Python extension builds.
-
-/bytecode_triplet
-	The `-` separated strings representing the bytecode used by the executing Python
-	context.
 """
 import sys
-import imp
 import types
+import typing
 import importlib
 
 from ..routes import library as libroutes
-
-selected_context = 'host'
-selected_role = 'optimal'
-
-def python_context(implementation, version_info, abiflags, platform):
-	"""
-	Construct the triplet representing the Python context for the platform.
-	Used to define the construction context for Python extension modules.
-	"""
-	pyversion = ''.join(map(str, version_info[:2]))
-	return '-'.join((implementation, pyversion + abiflags, platform))
-
-# Used as the context name for extension modules.
-python_triplet = python_context(
-	sys.implementation.name, sys.version_info, sys.abiflags, sys.platform
-)
-
-bytecode_triplet = python_context(
-	sys.implementation.name, sys.version_info, '', 'bytecode'
-)
-
-selections = None
-
-_factor_role_patterns = None
-_factor_roles = None # exact matches
-
-def select(module, role, context=None):
-	"""
-	Designate that the given role should be used for the identified &package and its content.
-
-	&select should only be used during development or development related operations. Notably,
-	selecting the role for a given package during the testing of a project.
-
-	It can also be used for one-off debugging purposes where a particular target is of interest.
-	"""
-	global _factor_roles, _factor_role_patterns
-	if _factor_roles is None:
-		_factor_roles = {}
-
-	if module.endswith('.'):
-		path = tuple(module.split('.')[:-1])
-		from ..computation import libmatch
-
-		if _factor_role_patterns is None:
-			_factor_role_patterns = libmatch.SubsequenceScan([path])
-		else:
-			x = list(_factor_role_patterns.sequences)
-			x.append(path)
-			_factor_role_patterns = libmatch.SubsequenceScan(x)
-
-		_factor_roles[module[:-1]] = role
-	else:
-		# exact
-		_factor_roles[module] = role
-
-def role(module, role='optimal', context=None):
-	"""
-	Get the configured role for the given module path.
-	"""
-	global _factor_roles, _factor_role_patterns
-
-	path = str(module)
-
-	if _factor_roles is None:
-		return role
-
-	if path in _factor_roles:
-		return _factor_roles[path]
-
-	if _factor_role_patterns is not None:
-		# check for pattern
-		path = _factor_role_patterns.get(tuple(path.split('.')))
-		return _factor_roles['.'.join(path)]
-
-	return default_role
 
 def canonical_name(route:libroutes.Import):
 	"""
@@ -126,14 +32,6 @@ def canonical_name(route:libroutes.Import):
 	r.reverse()
 	return '.'.join(r)
 
-def cache_directory(module, context, role, subject):
-	"""
-	Get the relevant context-role directory inside the associated
-	(fs-directory)`__pycache__` directory.
-	"""
-	# Get a route to a directory in __pycache__.
-	return libroutes.File.from_absolute(module.__file__).container / '__pycache__' / context / role / subject
-
 def extension_access_name(name:str) -> str:
 	"""
 	The name, Python module path, that the extension module will be available at.
@@ -149,17 +47,25 @@ def extension_access_name(name:str) -> str:
 def extension_composite_name(name:str) -> str:
 	"""
 	Given the name of a Python extension module, inject the identifier `'extension'`
-	between the package and module's identifier.
+	between the package and module's identifier giving the source factor of the
+	extension module.
 
-	[ Effects ]
-	/Product
+	[ Return ]
+
+	/&*Annotation
 		A string referring to a (module) composite factor.
 	"""
 	root = str(libroutes.Import.from_fullname(name).floor())
 	return '.'.join((root, 'extensions', name[len(root)+1:]))
 
-def package_directory(import_route:libroutes.Import):
+def package_directory(import_route:libroutes.Import) -> libroutes.File:
 	return import_route.file().container
+
+def inducted(factor:libroutes.Import) -> libroutes.File:
+	"""
+	Return the &libroutes.File instance to the inducted target.
+	"""
+	return (factor.file().container / '__pycache__' / 'factor')
 
 def sources(factor:libroutes.Import, dirname='src', module=None):
 	"""
@@ -177,13 +83,6 @@ def sources(factor:libroutes.Import, dirname='src', module=None):
 
 	return pkgdir / dirname
 
-def work(module:libroutes.Import, context:str, role:str):
-	"""
-	Return the work directory of the factor &module for the given &context and &role.
-	"""
-	path = package_directory(module) / context / role
-	return libroutes.File(None, path.absolute)
-
 def composite(factor:libroutes.Import):
 	"""
 	Whether the given &factor reference is a composite. &factor must be a real route.
@@ -200,29 +99,24 @@ def composite(factor:libroutes.Import):
 	return True
 
 def probe(module:types.ModuleType):
+	"""
+	Whether the module is declared to be a system probe.
+	"""
 	return module.__factor_type__ == 'system.probe'
 
-def reduction(composite:libroutes.Import, context=None, role=None, module=None):
-	"""
-	The reduction of the &composite.
-
-	The file is relative to the Python cache directory of the package module
-	identifying itself as a system module: (fs-path)`{context}/{role}/factor`.
-	"""
-	if module is not None:
-		pkgdir = libroutes.File.from_absolute(module.__file__).container
-	else:
-		pkgdir = composite.file().container
-
-	return pkgdir / '__pycache__' / context / role / 'factor'
-
-def dependencies(factor:types.ModuleType):
+def dependencies(factor:types.ModuleType) -> typing.Iterable[types.ModuleType]:
 	"""
 	Collect and yield a sequence of dependencies identified by
 	the dependent's presence in the module's globals.
 
 	This works on the factor's module object as the imports performed
 	by the module body make up the analyzed data.
+
+	[ Return ]
+
+	/&*Annotation
+		An iterable producing modules referenced by &factor that have
+		explicitly defined the (python:attribute)`__factor_type__` name.
 	"""
 	ModuleType = types.ModuleType
 
@@ -230,7 +124,11 @@ def dependencies(factor:types.ModuleType):
 		if isinstance(v, ModuleType) and getattr(v, '__factor_type__', None) is not None:
 			yield v
 
-def python_extension(module):
+def python_extension(module) -> bool:
+	"""
+	Determine if the given package module represents a Python extension by
+	analyzing its dependencies.
+	"""
 	for x in module.__dict__.values():
 		if isinstance(x, types.ModuleType):
 			if getattr(x, 'context_extension_probe', False):
