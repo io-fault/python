e3212a4e4b42db97c8bc9292964b990e70e6877c
fault <critical@fault.io>
2017-05-21 11:25:17 -0700

    Documentation.

--- route/__init__.py
+++ route/__init__.py
@@ -4,10 +4,10 @@
 # fetching modification times convenient and obvious.
 
 # A route, &.library.Route, is merely a sequence of identifiers used to select an object or
-# concept. However, it is structured so that the path is relative to a context route in
+# concept. However, it is structured so that the path is relative to a context in
 # order to make certain manipulations relative to a particular node.
 # Importantly, nodes may not actually exist; the path of the route is entirely independent
-# of system managing nodes.
+# of connected system managing the Real Nodes.
 
 #!/pl/python
 	from fault.chronometry import library as libtime
@@ -15,26 +15,15 @@
 
 	cat_exe = libroutes.File.from_absolute('/bin/cat')
 	etc_dir = libroutes.File.from_absolute('/etc')
+	root_dir = libroutes.File.from_absolute('/')
 
-# [ File System ]
+	# # Queries.
+	mod_time = cat_exe.get_last_modified()
+	binary_size = cat_exe.size()
+	assert cat_exe.type() == 'executable'
+	assert root_dir.type() == 'directory'
 
-# /Modification Time
-	# &.library.File.last_modified
-	#!/pl/python
-		mod_time = cat_exe.get_last_modified()
-
-	# However, the &.library.File.since method likely to be more common.
-	#!/pl/python
-		modified_files = etc_dir.since(libtime.now().rollback(hour=1))
-
-# /Size
-	# &.library.File.size
-	#!/pl/python
-		binary_size = cat_exe.size()
-
-# /Type
-	# &.library.File.type
-	#!/pl/python
-		assert cat_exe.type() == 'executable'
+	# # Get changes since:
+	modified_files = etc_dir.since(libtime.now().rollback(hour=1))
 """
 __factor_type__ = 'project'
