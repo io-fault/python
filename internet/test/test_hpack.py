6a4a22d1765be552cf50bb7e913a08acaa29813d
fault <critical@fault.io>
2017-05-21 11:29:04 -0700

    Add test using samples from the RFC

--- internet/test/test_hpack.py
+++ internet/test/test_hpack.py
@@ -16,6 +16,36 @@ def test_huffman_coding(test):
         coded = module.huffman_encode(x)
         test/x == module.huffman_decode(coded)
 
+def test_huffman_samples(test):
+	"""
+	# Tests comprised of data from examples in
+	# &<http://httpwg.org/specs/rfc7541.html>
+	"""
+	test/module.huffman_decode(b'\xae\xc3\x77\x1a\x4b') == b'private'
+	test/module.huffman_encode(b'private') == b'\xae\xc3\x77\x1a\x4b'
+
+	i = b''.join([
+		b'\xd0\x7a\xbe\x94\x10',
+		b'\x54\xd4\x44\xa8\x20',
+		b'\x05\x95\x04\x0b\x81',
+		b'\x66\xe0\x82\xa6\x2d',
+		b'\x1b\xff'
+	])
+	out = b'Mon, 21 Oct 2013 20:13:21 GMT'
+	test/module.huffman_decode(i) == out
+	test/module.huffman_encode(out) == i
+
+	j = b''.join([
+		b'\x9d\x29\xad\x17',
+		b'\x18\x63\xc7\x8f',
+		b'\x0b\x97\xc8\xe9',
+		b'\xae\x82\xae\x43',
+		b'\xd3'
+	])
+	out = b'https://www.example.com'
+	test/module.huffman_decode(j) == out
+	test/module.huffman_encode(out) == j
+
 if __name__ == '__main__':
     import sys
     from ...development import libtest
