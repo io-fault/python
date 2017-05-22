70ac1c08a1fe79f116d6e3ba2bcb420d33ade789
fault <critical@fault.io>
2017-05-21 20:56:34 -0700

    Documentation.

--- time/project.py
+++ time/project.py
@@ -1,4 +1,4 @@
-identity = 'http://fault.io/python/chronometry'
+identity = 'http://fault.io/src/fault.chronometry'
 name = 'chronometry'
 abstract = 'An integer based time solution with selectable precision'
 icon = '⌛'
