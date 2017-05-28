c797be429b12f0d33c101cdcbd75afdefead0378
fault <critical@fault.io>
2017-05-28 11:07:37 -0700

    Add libfactor.composite test.

--- system/test/test_libfactor.py
+++ system/test/test_libfactor.py
@@ -1,10 +1,17 @@
-from .. import libfactor as library
+from .. import libfactor as module
+from ...routes import library as libroutes
 
 def test_composite(test):
 	"""
 	# Validate &library.composite
 	"""
-	test.warning("not implemented")
+
+	test/module.composite(libroutes.Import.from_fullname(__name__)) == False
+	test/module.composite(libroutes.Import.from_fullname(__package__)) == False
+
+	prefix = module.__package__ + '.'
+	ir = libroutes.Import.from_fullname(prefix + 'extensions.kernel')
+	test/module.composite(ir) == True
 
 if __name__ == '__main__':
 	from .. import libtest; import sys
