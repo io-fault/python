23b3f2024ed875e3c52435115077e76872c9dcc0
fault <critical@fault.io>
2017-12-07 14:02:27 -0700

    Use requirement specification isntead of probe module.

--- security/extensions/openssl/__init__.py
+++ security/extensions/openssl/__init__.py
@@ -1,8 +1,11 @@
 """
 # OpenSSL bindings for supporting asynchronous I/O.
 """
-from ....development import ipython
-from ...probes import openssl # provides environment parameters for openssl
-
 __factor_domain__ = 'system'
 __factor_type__ = 'extension'
+
+from ...probes import openssl # provides environment parameters for openssl
+requirements = (
+	'python',
+)
+
