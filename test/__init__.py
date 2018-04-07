f283a5ecea3a919e82d5f961bac2c1fee9271d22
fault <critical@fault.io>
2018-04-06 23:07:08 -0700

    Initial commit moving originals from fault.development/kit.integrate

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+"""
+# Primitives for writing and executing tests for software.
+"""
+__factor_type__ = 'project'
---
+++ test/project.py
@@ -0,0 +1,11 @@
+identity = 'https://fault.io/src/python/test'
+name = 'test'
+abstract = ''
+icon = ''
+
+fork = 'columns'
+versioning = 'continuous'
+status = 'flux'
+
+controller = 'fault.io'
+contact = 'mailto:critical@fault.io'
