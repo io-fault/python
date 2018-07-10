a3f0923ebc84ebf763df7987cb44a7e63e8fd7bf
fault <critical@fault.io>
2018-07-10 15:45:52 -0700

    Add new path location for system files

---
+++ system/files.py
@@ -0,0 +1,10 @@
+"""
+# Filesystem interfaces and data structures.
+
+# [ Engineering ]
+# This is the future home of &fault.routes.library.File as routes takes on
+# its new primary responsibility of managing the route interfaces and data structures
+# for point management.
+"""
+
+from ..routes.library import File as Path
