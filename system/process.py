131207853022ed1c86775dc3039acae44b197915
fault <critical@fault.io>
2018-07-17 22:48:49 -0700

    Add stubs for future locations

---
+++ system/process.py
@@ -0,0 +1,8 @@
+"""
+# [ Engineering ]
+# Future location of a large portion of &.library.
+"""
+
+from .library import Invocation
+from .library import Exit
+from .library import control
---
+++ system/python.py
@@ -0,0 +1,8 @@
+"""
+# Python Execution Context support functions and classes.
+
+# [ Engineering ]
+# Future home of &..routes.library.Import.
+"""
+
+from ..routes.library import Import
