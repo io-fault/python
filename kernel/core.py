d2c4a6250d9a5efc35c39e7865012840cca10afd
fault <critical@fault.io>
2019-02-17 01:02:25 -0700

    Relocate Join and Condition

--- kernel/core.py
+++ kernel/core.py
@@ -4,6 +4,103 @@
 import builtins
 import operator
 
+class Join(object):
+	"""
+	# An object whose purpose is to join the completion of multiple
+	# processors into a single event. Joins are used to simplify coroutines
+	# whose progression depends on a set of processors instead of one.
+
+	# Joins also enable interrupts to trigger completion events so that
+	# failures from unrelated Sectors can be communicated to callback.
+
+	# [ Properties ]
+
+	# /dependencies/
+		# The original set of processors as a dictionary mapping
+		# given names to the corresponding &Processor.
+
+	# /pending/
+		# The current state of pending exits that must
+		# occur prior to the join-operation's completion.
+
+	# /callback/
+		# The callable that is performed after the &pending
+		# set has been emptied; defined by &atexit.
+	"""
+
+	__slots__ = ('dependencies', 'pending', 'callback')
+
+	def __init__(self, **processors):
+		"""
+		# Initialize the join with the given &processor set.
+		"""
+
+		self.dependencies = processors
+		self.pending = set(processors.values())
+		self.callback = None
+
+	def connect(self):
+		"""
+		# Connect the &Processor.atexit calls of the configured
+		# &dependencies to the &Join instance.
+		"""
+
+		for x in self.dependencies.values():
+			x.atexit(self.exited)
+
+		return self
+
+	def __iter__(self, iter=iter):
+		"""
+		# Return an iterator to the configured dependencies.
+		"""
+
+		return iter(self.dependencies.values())
+
+	def __getitem__(self, k):
+		"""
+		# Get the dependency the given identifier.
+		"""
+
+		return self.dependencies[k]
+
+	def exited(self, processor):
+		"""
+		# Record the exit of the given &processor and execute
+		# the &callback of the &Join if the &processor is the last
+		# in the configured &pending set.
+		"""
+
+		self.pending.discard(processor)
+
+		if not self.pending:
+			# join complete
+			self.pending = None
+
+			cb = self.callback
+			self.callback = None; cb(self) # clear callback to signal completion
+
+	def atexit(self, callback):
+		"""
+		# Assign the callback of the &Join.
+
+		# If the &pending set is empty, the callback will be immediately executed,
+		# otherwise, overwrite the currently configured callback.
+
+		# The &callback is executed with the &Join instance as its sole parameter.
+
+		# [ Parameters ]
+
+		# /callback/
+			# The task to perform when all the dependencies have exited.
+		"""
+
+		if self.pending is None:
+			callback(self)
+			return
+
+		self.callback = callback
+
 class Condition(object):
 	"""
 	# A *reference* to a logical expression or logical function.
--- kernel/library.py
+++ kernel/library.py
@@ -39,46 +39,6 @@ from . import system
 
 __shortname__ = 'libkernel'
 
-def parse_transport_indicator(ti:str, port = None):
-	"""
-	# Parse a Transport Indicator for constructing connection templates.
-	"""
-	parts = ri.parse(tri)
-
-	hn = parts['host']
-	*leading, primary = hn.split('.')
-
-	parts['category'] = primary
-
-	# octets+flows, if any. Indicates a transition from
-	# an octets stream to a set of flows.
-	transitions = tuple(parts['scheme'].split('+'))
-	if len(transitions) > 1:
-		parts['transitions'] = transitions
-	else:
-		parts['transitions'] = ()
-
-	if primary == 'index':
-		# Address Resolution of some sort. Usually GetAddressInfo.
-		entry = parts['path'][0]
-		service = parts.get('port', port)
-	elif primary == 'kernel':
-		# Only one level supported. 'port' and 'file'
-		kd = parts['kdomain'], = leading
-		kt = parts['ktype'] = parts['path'][0]
-		if kt == 'file':
-			try:
-				parts['kmode'] = parts['path'][1]
-			except IndexError:
-				parts['kmode'] = 'read'
-		else:
-			parts['kmode'] = None
-	else:
-		# version selector. Remove leading 'v' and replace '-' with '.'.
-		parts['version'] = leading[-1][1:].replace('-', '.')
-
-	return parts
-
 class Lock(object):
 	"""
 	# Event driven lock.
@@ -478,103 +438,6 @@ endpoint_classes = {
 	'domain': libnet.Reference.from_domain,
 }
 
-class Join(object):
-	"""
-	# An object whose purpose is to join the completion of multiple
-	# processors into a single event. Joins are used to simplify coroutines
-	# whose progression depends on a set of processors instead of one.
-
-	# Joins also enable interrupts to trigger completion events so that
-	# failures from unrelated Sectors can be communicated to callback.
-
-	# [ Properties ]
-
-	# /dependencies/
-		# The original set of processors as a dictionary mapping
-		# given names to the corresponding &Processor.
-
-	# /pending/
-		# The current state of pending exits that must
-		# occur prior to the join-operation's completion.
-
-	# /callback/
-		# The callable that is performed after the &pending
-		# set has been emptied; defined by &atexit.
-	"""
-
-	__slots__ = ('dependencies', 'pending', 'callback')
-
-	def __init__(self, **processors):
-		"""
-		# Initialize the join with the given &processor set.
-		"""
-
-		self.dependencies = processors
-		self.pending = set(processors.values())
-		self.callback = None
-
-	def connect(self):
-		"""
-		# Connect the &Processor.atexit calls of the configured
-		# &dependencies to the &Join instance.
-		"""
-
-		for x in self.dependencies.values():
-			x.atexit(self.exited)
-
-		return self
-
-	def __iter__(self, iter=iter):
-		"""
-		# Return an iterator to the configured dependencies.
-		"""
-
-		return iter(self.dependencies.values())
-
-	def __getitem__(self, k):
-		"""
-		# Get the dependency the given identifier.
-		"""
-
-		return self.dependencies[k]
-
-	def exited(self, processor):
-		"""
-		# Record the exit of the given &processor and execute
-		# the &callback of the &Join if the &processor is the last
-		# in the configured &pending set.
-		"""
-
-		self.pending.discard(processor)
-
-		if not self.pending:
-			# join complete
-			self.pending = None
-
-			cb = self.callback
-			self.callback = None; cb(self) # clear callback to signal completion
-
-	def atexit(self, callback):
-		"""
-		# Assign the callback of the &Join.
-
-		# If the &pending set is empty, the callback will be immediately executed,
-		# otherwise, overwrite the currently configured callback.
-
-		# The &callback is executed with the &Join instance as its sole parameter.
-
-		# [ Parameters ]
-
-		# /callback/
-			# The task to perform when all the dependencies have exited.
-		"""
-
-		if self.pending is None:
-			callback(self)
-			return
-
-		self.callback = callback
-
 class ExceptionStructure(object):
 	"""
 	# Exception associated with an interface supporting the sequencing of processor trees.
@@ -629,8 +492,7 @@ class Projection(object):
 
 	def __init__(self):
 		"""
-		Projections are simple data structures and requires no initialization
-		parameters.
+		# Projections are simple data structures and requires no initialization.
 		"""
 
 class Layer(object):
@@ -2721,63 +2583,6 @@ class Network(Context):
 			series = ctx_accept(fd, mitre, mitre.Protocol())
 			cxn = Transaction.create(Transport(series))
 			sector.dispatch(cxn) # actuate and assign the connection
-
-class Condition(object):
-	"""
-	# A *reference* to a logical expression or logical function.
-
-	# Conditional references are constructed from a subject object, attribute path, and parameters.
-	# Used to clearly describe the objects that participate in a logical conclusion of interest.
-
-	# Used by &Flow instances to describe the condition in which an obstruction is removed.
-	# Conditions provide introspecting utilities the capacity to identify the cause of
-	# an obstruction.
-	"""
-
-	__slots__ = ('focus', 'path', 'parameter')
-
-	def __init__(self, focus, path, parameter = None):
-		"""
-		# [ Parameters ]
-		# /focus/
-			# The root object that is safe to reference
-		# /path/
-			# The sequence of attributes to resolve relative to the &focus.
-		# /parameter/
-			# Determines the condition is a method and should be given this
-			# as its sole parameter. &None indicates that the condition is a property.
-		"""
-		self.focus = focus
-		self.path = path
-		self.parameter = parameter
-
-	def __bool__(self):
-		condition = self.attribute()
-
-		if self.parameter is not None:
-			return condition(self.parameter)
-		else:
-			# property
-			return condition
-
-	def __repr__(self):
-		if self is Inexorable:
-			return 'Inexorable'
-
-		try:
-			attval = self.attribute()
-		except:
-			attval = '<exception>'
-
-		return "<Condition [%r].%s == %r>" %(
-			self.focus, '.'.join(self.path), attval
-		)
-
-	def attribute(self, ag=operator.attrgetter):
-		return ag('.'.join(self.path))(self.focus)
-
-# A condition that will never be true.
-Inexorable = Condition(builtins, ('False',))
 
 ProtocolTransactionEndpoint = typing.Callable[[
 	Processor, Layer, Layer, typing.Callable[[Processor], None]
