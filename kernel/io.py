178d9bdb4c19da11fb277236aef55c46f5a96466
fault <critical@fault.io>
2019-04-28 21:13:30 -0700

    Relocate and remove some bitrot.
    Further effort of the elimination of library.py.
    endpoint interfaces may only exist temporarily in io.py.

--- kernel/io.py
+++ kernel/io.py
@@ -7,9 +7,13 @@
 """
 import typing
 import itertools
+import functools
 
 from . import core
 from . import flows
+
+from ..internet import ri
+from ..internet import host
 
 class Transfer(core.Context):
 	"""
@@ -216,6 +220,65 @@ class Interface(core.Executable):
 			flow.f_connect(if_t)
 			flow.f_transfer(None) # Start allocating file descriptor arrays.
 
+class Accept(Transfer):
+	"""
+	# The Transfer Context used to accept and route sockets from listening interfaces.
+
+	# [ Engineering ]
+	# In line to replace &Network, Accept is a Transport Context that fills
+	# the same role, but properly identified as a Context, not an Interface.
+	# It is still not certain whether Accept should handle more than one
+	# Flow. sockets://127.0.0.1:8484/tls/http
+	"""
+
+	def at_route(self, packet, chain=itertools.chain.from_iterable):
+		"""
+		# Moving to Sockets. Routing will be handled with flows.
+		"""
+		sector = self.controller
+		ctx_accept = self.context.accept_subflows
+
+		source, event = packet
+		for fd in chain(event):
+			mitre = self.if_mitre(self.if_reference, self.if_router)
+			series = ctx_accept(fd, mitre, mitre.Protocol())
+			cxn = Transaction.create(Transport(series))
+			sector.dispatch(cxn) # actuate and assign the connection
+
+	def at_install(self, *kports):
+		"""
+		# Given file descriptors, install a new set of flows
+		# accepting sockets from the given listening sockets.
+		"""
+		ctx = self.context
+		sector = self.controller
+
+		for listen in ctx.acquire_listening_sockets(kports):
+			x, flow = listen
+			sector.dispatch(flow)
+
+			if_r = (x.interface, x.port)
+			if_t = Sockets(if_r, self.at_route)
+			sector.dispatch(if_t)
+			if_t.f_connect(null)
+
+			flow.f_connect(if_t)
+
+			add(if_r)
+			flow.process(None) # Start allocating file descriptor arrays.
+
+	def at_bind(self, slot):
+		"""
+		# Bind the Kernel Ports associated with the given slot.
+		# Allocates duplicate file descriptors from `/dev/ports`,
+		# and installs them in the Transaction.
+		"""
+		ctx = self.context
+		ports = ctx.association().ports
+
+		fds = ports.acquire(self.if_slot)
+		self.at_install(*fds)
+
 def security_operations(transport):
 	"""
 	# Construct the input and output operations used by &.library.Transports instances.
@@ -238,3 +301,185 @@ def dispatch(xact, *flows:typing.Sequence[flows.Channel]):
 		sub = Transaction.create(xf)
 		xact.dispatch(sub)
 		xf.critical(functools.partial(xf.io_flow, *x))
+
+class Local(tuple):
+	"""
+	# A reference to a unix domain file system socket.
+
+	# While local connections do not have "addresses", &..io generalizes
+	# all endpoints regardless of transport. In the case of local sockets,
+	# the directory containing the socket file is known as the `address`,
+	# and the filename is known as the `port`.
+
+	# All local socket connections are said to transfer "octets" and
+	# are semantically consistent with TCP/IP connections.
+	"""
+
+	__slots__ = ()
+
+	@property
+	def protocol(self):
+		return 'local'
+
+	@property
+	def interface(self):
+		"""
+		# Directory containing the file system socket.
+		"""
+
+		return self[0]
+	address = interface
+
+	@property
+	def port(self):
+		"""
+		# File system socket filename.
+		"""
+
+		return self[1]
+
+	@property
+	def route(self):
+		return files.Path.from_absolute(self[0]) / self[1]
+
+	@classmethod
+	def create(Class, directory, file):
+		return Class((directory, file))
+
+	def __str__(self):
+		return '[' + (self[0].rstrip('/') + '/') +self[1]+']'
+
+class Endpoint(tuple):
+	"""
+	# A process-local endpoint. These objects are pointers to [logical] process resources.
+	"""
+
+	__slots__ = ()
+	protocol = 'rs' # Process[or Unit] Space
+
+	@property
+	def unit(self):
+		"""
+		# The absolute unit name; &None if subjective reference.
+		"""
+
+		return self[0]
+
+	@property
+	def pid(self):
+		"""
+		# The process identifier pointing to the location of the endpoint.
+		# Necessary in interprocess communication.
+		"""
+
+		return self[4]
+
+	@property
+	def path(self):
+		"""
+		# The path in the structure used to locate the container.
+		"""
+
+		if not self.directory:
+			return self[1][:-1]
+
+	@property
+	def identifier(self):
+		"""
+		# Last component in the path if it's not a directory.
+		"""
+
+		if not self.directory:
+			return self[1][-1]
+
+	@property
+	def directory(self):
+		"""
+		# Endpoint refers to the *directory* of the location, not the assigned object.
+		"""
+
+		return self[2]
+
+	@property
+	def validation(self):
+		"""
+		# A unique identifier selecting an object within the &Resource.
+		# Usually the result of an &id call of a particular object
+		"""
+
+		return self[3]
+
+	def __str__(self, formatting = "{0}{4}/{1}{2}{3}"):
+		one = '/'.join(self.path)
+		three = two = ''
+
+		if self.directory:
+			two = '/'
+		if self.validation is not None:
+			three = '#' + str(self.validation)
+
+		if self.program:
+			zero = "rs://" + self.unit
+		else:
+			zero = "/"
+
+		if self.pid is not None:
+			four = ":" + str(self.pid)
+
+		return formatting.format(zero, one, two, three, four)
+
+	@classmethod
+	def parse(Class, psi):
+		"""
+		# Parse an IRI-like indicator for selecting a process object.
+		"""
+
+		dir = False
+		d = ri.parse(psi)
+
+		path = d.get('path', ())
+		if path != ():
+			if path[-1] == '':
+				pseq = path[:-1]
+				dir = True
+			else:
+				pseq = path
+		else:
+			pseq = ()
+
+		port = d.get('port', None)
+
+		return Class(
+			(d['host'], tuple(pseq), dir, d.get('fragment', None), port)
+		)
+
+	@classmethod
+	def local(Class, *path, directory = False):
+		"""
+		# Construct a local reference using the given absolute path.
+		"""
+
+		return Class((None, path, directory, None, None))
+
+endpoint_classes = {
+	'local': Local.create,
+	'ip4': host.Endpoint.create_ip4,
+	'ip6': host.Endpoint.create_ip6,
+	'domain': host.Reference.from_domain,
+}
+
+@functools.lru_cache(32)
+def endpoint(type:str, address:str, port:object):
+	"""
+	# Endpoint constructor for fault.io applicaitons.
+
+	# [ Samples ]
+	# /IPv4/
+		# `libkernel.endpoint('ip4', '127.0.0.1', 80)`
+	# /IPv6/
+		# `libkernel.endpoint('ip6', '::1', 80)`
+	# /Local/
+		# `libkernel.endpoint('local', '/directory/path/to', 'socket_file')`
+	"""
+
+	return endpoint_classes[type](address, port)
