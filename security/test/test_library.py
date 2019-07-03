de9599a73046b827f8e8895b0a08387b71b6305e
fault <critical@fault.io>
2019-07-03 11:30:38 -0700

    Unused

--- security/test/test_library.py
+++
@@ -1,8 +0,0 @@
-from .. import library
-
-def test_feature(test):
-	test/'foo' == 'foo'
-
-if __name__ == '__main__':
-	import sys; from ...test import library as libtest
-	libtest.execute(sys.modules[__name__])
