075597a89e36ad68a3a40b40157bb778e804aaa5
fault <critical@fault.io>
2019-07-03 20:33:49 -0700

    Relocate from computation.

---
+++ context/string.py
@@ -0,0 +1,54 @@
+"""
+# Supplemental string operations.
+"""
+import itertools
+import operator
+
+def indent(string:str, level:int=1, indentwith:str='\t') -> str:
+	"""
+	# Indent the given &string using the &level * &indentwith.
+	"""
+
+	lines = string.splitlines(True)
+	i = (level * indentwith)
+
+	return (i if lines else i.__class__()) + i.join(lines)
+
+def normal(string:str, separator:str=' ') -> str:
+	"""
+	# Normalize the whitespace in the given string.
+	"""
+
+	return separator.join((x for x in string.split() if not x.isspace()))
+
+def slug(string:str, separators=str.maketrans('_', ' '), replacement:str='-') -> str:
+	"""
+	# Convert &separators and spaces into single &replacement instances inside &string.
+	"""
+	return normal(string.translate(separators), replacement)
+
+def varsplit(indicator, string):
+	"""
+	# str.split() translating series of tokens into numbers instead
+	# of mere separations. The returned list has the pattern: `[str, int, str, int, ...]`.
+	"""
+	parts = iter(string.split(indicator))
+
+	yield next(parts)
+
+	count = 1
+	for x in parts:
+		if x:
+			yield count
+			yield x
+			count = 1
+		else:
+			count += 1
+	else:
+		# substract the implicit increment on the tail.
+		count -= 1
+
+	# trailing indicators
+	if count:
+		yield count
+		yield ""
---
+++ context/test/test_string.py
@@ -0,0 +1,41 @@
+from .. import string as module
+
+def test_normal(test):
+	test/module.normal("Nothing happens") == "Nothing happens"
+	test/module.normal("One space  trimmed") == "One space trimmed"
+	test/module.normal("Two  spaces  trimmed") == "Two spaces trimmed"
+	test/module.normal(" Four  spaces  trimmed ") == "Four spaces trimmed"
+
+def test_slug(test):
+	test/module.slug("A simple Id") == "A-simple-Id"
+
+	# Surrounding whitespace immune.
+	test/module.slug(" A simple Id") == "A-simple-Id"
+	test/module.slug(" A simple Id ") == "A-simple-Id"
+
+def test_varsplit(test):
+	def split(*args):
+		return list(module.varsplit(*args))
+
+	# Maintain split()'s effect when no split occurs.
+	test/split('*', "Nothing") == ["Nothing"]
+
+	for x in range(1, 32):
+		test/split('*', "Something"+("*"*x)) == ["Something", x, ""]
+
+	for x in range(1, 32):
+		test/split('*', "Something"+("*"*x)) == ["Something", x, ""]
+
+	for x in range(1, 32):
+		test/split(
+			'*', "Something" + ("*"*x) + "Following"
+		) == ["Something", x, "Following"]
+
+	for x in range(1, 32):
+		test/split(
+			'*', "Something" + ("*"*x) + "Following" + ("*"*x)
+		) == ["Something", x, "Following", x, '']
+
+if __name__ == '__main__':
+	from ...test import library as libtest
+	import sys; libtest.execute(sys.modules[__name__])
