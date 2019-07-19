a9c9debc5eb813b8b3f298c6d0fc999c321f2b45
fault <critical@fault.io>
2019-07-18 21:06:51 -0700

    Implement Protocol subclasses for managing secured I/O.

---
+++ security/kprotocol.py
@@ -0,0 +1,78 @@
+"""
+# &..kernel protocol adapters for transport security contexts.
+"""
+
+import weakref
+
+from ..kernel import flows
+
+class SecuredTransmit(flows.Protocol):
+	"""
+	# Transport stack entry for secured output.
+	# Holds a weak reference to the corresponding &SecuredReceive instance
+	# using &stx_receive_channel.
+	"""
+
+	_stx_receive_closed = False
+
+	def stx_receive_closed(self):
+		"""
+		# Signal endpoint recognizing when the corresponding receive
+		# has seen a transport protocol level termination.
+		"""
+		self._stx_receive_closed = True
+
+		if self.terminating:
+			self._f_terminated()
+
+	def stx_receive_interrupt(self):
+		"""
+		# Wire closed without protocol shutdown.
+		"""
+		self._stx_receive_closed = True
+
+		if self.terminating:
+			self._f_terminated()
+
+	def f_terminate(self):
+		if not self.functioning:
+			return
+
+		self.start_termination()
+		self.p_shared.close()
+		self.p_drain()
+
+class SecuredReceive(flows.Protocol):
+	"""
+	# Transport stack entry for secured input.
+	# Holds a strong reference to the corresponding &SecuredTransmit instance
+	# using &srx_transmit_channel.
+	"""
+
+	def p_terminated(self):
+		self.start_termination()
+		self.srx_transmit_channel.stx_receive_closed()
+
+	def f_terminate(self):
+		if not self.terminating:
+			self.srx_transmit_channel.stx_receive_interrupt()
+		self._f_terminated()
+
+def allocate(tls, Method=weakref.WeakMethod, Reference=weakref.ref):
+	"""
+	# Construct a protocol stack pair using the given &tls instance.
+	"""
+
+	stx = SecuredTransmit(tls, None, tls.encipher)
+	srx = SecuredReceive(tls, None, tls.decipher)
+
+	srx.srx_transmit_channel = stx
+	stx.stx_receive_channel = Reference(srx)
+
+	drain = Method(stx.p_drain)
+	tls.connect_transmit_ready((lambda: drain()()))
+
+	ptermd = Method(srx.p_terminated)
+	tls.connect_receive_closed((lambda: ptermd()()))
+
+	return (srx, stx)
---
+++ security/test/test_kprotocol.py
@@ -0,0 +1,108 @@
+"""
+# Validate protocol implementations.
+"""
+from .. import kprotocol as module
+
+class TLS:
+	closed = False
+	ri = False
+
+	def encipher(self, data):
+		return ('secret', data)
+
+	def decipher(self, data):
+		return ('reveal', data)
+
+	def close(self):
+		self.closed = True
+
+	def receive_interrupt(self):
+		self.ri = True
+
+	def connect_transmit_ready(self, cb):
+		self.tr = cb
+
+	def connect_receive_closed(self, cb):
+		self.rc = cb
+
+def test_allocate(test):
+	srx, stx = module.allocate(TLS())
+
+	# Check cross references.
+	test/srx.srx_transmit_channel == stx
+	test/stx.stx_receive_channel() == srx
+
+def test_transfers(test):
+	"""
+	# Validate encrypt and decrypt methods.
+	"""
+	srx, stx = module.allocate(TLS())
+	srx.f_emit = (lambda x: x);
+	stx.f_emit = (lambda x: x);
+
+	test/srx.f_transfer(b"test") == ('reveal', b"test")
+	test/stx.f_transfer(b"test") == ('secret', b"test")
+
+def test_channel_interrupt(test):
+	"""
+	# - &module.SecuredTransmit.stx_receive_interrupt
+	"""
+	shared = TLS()
+	srx, stx = module.allocate(shared)
+
+	srx.exit = (lambda: None)
+	stx.exit = (lambda: None)
+
+	srx.f_emit = (lambda x: x)
+	stx.f_emit = (lambda x: x)
+
+	test/srx.f_terminate()
+	test/stx._stx_receive_closed == True
+	test/shared.ri == True
+
+def test_channel_terminate_callback(test):
+	"""
+	# - &module.SecuredTransmit.stx_receive_closed
+	"""
+	shared = TLS()
+	srx, stx = module.allocate(shared)
+
+	srx.exit = (lambda: None)
+	stx.exit = (lambda: None)
+
+	srx.f_emit = (lambda x: x)
+	stx.f_emit = (lambda x: x)
+
+	shared.rc()
+	test/srx.terminating == True
+	test/stx._stx_receive_closed == True
+
+def test_channel_terminate_full(test):
+	"""
+	# - &module.SecuredTransmit.stx_receive_closed
+	"""
+	shared = TLS()
+	srx, stx = module.allocate(shared)
+	termd = set()
+	srx._pexe_state = 1
+	stx._pexe_state = 1
+
+	srx.exit = (lambda: termd.add(srx))
+	stx.exit = (lambda: termd.add(stx))
+
+	srx.f_emit = (lambda x: x)
+	stx.f_emit = (lambda x: x)
+
+	stx.f_terminate()
+	test/stx.terminating == True
+	stx._f_terminated()
+	shared.rc()
+
+	test/stx._stx_receive_closed == True
+	test/stx.terminated == True
+	srx.f_terminate()
+	test/srx.terminated == True
+	test/shared.closed == True
+
+if __name__ == '__main__':
+	pass
