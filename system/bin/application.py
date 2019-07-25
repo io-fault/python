e29c0a50dbd291d56c7567fd779148a14c1db273
fault <critical@fault.io>
2019-07-24 20:49:24 -0700

    Add executable for application bindings that use argument-0 to select the module.

---
+++ system/bin/application.py
@@ -0,0 +1,14 @@
+"""
+# Execute an application module.
+"""
+import importlib
+from .. import process
+
+def main(inv:process.Invocation) -> process.Exit:
+	module_path = inv.parameters['system']['name']
+	sub = importlib.import_module(module_path)
+	process.Fork.substitute(sub.main, inv)
+	raise process.Panic("substitution failed to raised control exception")
+
+if __name__ == '__main__':
+	process.control(main, process.Invocation.system())
