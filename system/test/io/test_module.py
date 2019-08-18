4a5fc5f06028c6b034c005c1c07cd659665b0777
fault <critical@fault.io>
2019-08-17 23:31:59 -0700

    Eliminate test as there is no longer an array dependency.

--- system/test/io/test_module.py
+++ system/test/io/test_module.py
@@ -36,40 +36,6 @@ def test_no_subtyping(test):
 			class Foo(x):
 				pass
 
-def test_no_array(test):
-	import sys
-
-	kpath = '.'.join(__name__.split('.')[:-2]) + '.io'
-	test.skip(kpath in sys.modules)
-
-	import array
-	import types
-
-	t = types.ModuleType("array")
-	try:
-		sys.modules['array'] = t
-
-		try:
-			from ... import io
-			test.fail("import did not raise expected error")
-		except (ImportError, AttributeError):
-			pass
-
-		def err(*args):
-			raise Exception("nothin")
-		t.array = err
-
-		try:
-			from ... import io
-			test.fail("import did not raise expected error")
-		except Exception as exc:
-			test/str(exc) == "nothin"
-	finally:
-		sys.modules['array'] = array
-
-	with test.trap():
-		from ... import io
-
 if __name__ == '__main__':
 	import sys; from ....test import library as libtest
 	libtest.execute(sys.modules['__main__'])
