a4f1013f633e31815f7999850328931efc10a918
fault <critical@fault.io>
2020-08-01 04:48:22 -0700

    Add Cell type for recognizing singularity sequences.

---
+++ context/test/test_types.py
@@ -0,0 +1,68 @@
+from .. import types as module
+
+def test_Cell_iteration(test):
+	"""
+	# - &module.Cell.__iter__
+	"""
+	values = [0, 1, "text"]
+	for v in values:
+		c = module.Cell(v)
+		for x in c:
+			test/x == v
+
+def test_Cell_coalesce(test):
+	"""
+	# - &module.Cell.coalesce
+	"""
+	nothing = module.Cell(None)
+	test/nothing.coalesce(1) == 1
+	test/nothing.coalesce(None) == None
+
+	something = module.Cell("text")
+	test/something.coalesce(1) == "text"
+	test/something.coalesce(None) == "text"
+
+def test_Cell_sequence(test):
+	values = [0, 1, "text"]
+	for v in values:
+		c = module.Cell(v)
+		test/c.element == v
+		test/c[0] == v
+		test/list(c) == [v]
+
+		test/(v in c) == True
+		test/(None in c) == False
+
+		test/ValueError ^ (lambda: c.index(None))
+		test/ValueError ^ (lambda: c.index(v, start=1))
+		test/ValueError ^ (lambda: c.index(v, stop=0))
+		test/reversed(c) == c
+		test/c.index(v) == 0
+		test/c.count(v) == 1
+		test/c.count(None) == 0
+
+def test_Cell_equality(test):
+	"""
+	# - &module.Cell.__eq__
+	"""
+	values = [0, 1, "text"]
+	for v in values:
+		c = module.Cell(v)
+		test/c.element == v
+		test/c[0] == v
+		test/list(c) == [v]
+		test/reversed(c) == c
+
+def test_Cell_setup(test):
+	"""
+	# - &module.Cell.__setup__
+	"""
+	import collections.abc
+	module.Cell.__setup__()
+	c = module.Cell(None)
+	test.isinstance(c, collections.abc.Sequence)
+	test.isinstance(c, collections.abc.Iterable)
+
+if __name__ == '__main__':
+	from ...test import engine
+	import sys; engine.execute(sys.modules[__name__])
---
+++ context/types.py
@@ -0,0 +1,85 @@
+"""
+# Supplemental Python types.
+"""
+
+class Cell(object):
+	"""
+	# A Sequence implementation always containing a single element.
+
+	# [ Engineering ]
+	# The purpose of this type is to provide a means to allow receivers
+	# of a sequence to know that the sequence produced in a particular case
+	# will never contain more than one element without having to explicitly
+	# communicate such a fact with another value sent alongside the sequence.
+
+	# While an explicit plural field is clear and desirable, it also carries
+	# the burden of having to be propagated with the sequence.
+	# Having the sequence type designate the constraint allows the information
+	# to be propagated without concern by the nodes along the call graph.
+	"""
+	__slots__ = ('element',)
+
+	@classmethod
+	def __setup__(Class):
+		import collections.abc
+		collections.abc.Sequence.register(Class)
+		collections.abc.Iterable.register(Class)
+
+	def coalesce(self, default, abscence=None):
+		"""
+		# Return the given &default if the element is &abscence.
+
+		# By default, if the element is &None, return &default.
+		"""
+		if self.element is abscence:
+			return default
+		return self.element
+
+	def __init__(self, element:object):
+		self.element = element
+
+	def __getitem__(self, index):
+		if index != 0:
+			raise IndexError("cell only contains a single element")
+
+		return self.element
+
+	def __iter__(self):
+		yield self.element
+
+	def __reversed__(self):
+		return self
+
+	def __len__(self):
+		return 1
+
+	def __eq__(self, operand):
+		if len(operand) != 1:
+			return False
+		return self.element == operand[0]
+
+	def __contains__(self, item):
+		if item == self.element:
+			return True
+		return False
+
+	def index(self, value, start=0, stop=None):
+		if start != 0 or (stop is not None and stop <= 0):
+			raise ValueError("range not present in cell")
+
+		if value == self.element:
+			return 0
+
+		raise ValueError("cell element does not match the given value")
+
+	def count(self, value):
+		if value == self.element:
+			return 1
+		else:
+			return 0
+
+	def sort(self, key=None, reverse=False):
+		"""
+		# Sort the Cell's elements by doing nothing.
+		"""
+		pass
