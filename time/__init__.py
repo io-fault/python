ea41db040743a2b76901985e4d4067d82e7b5303
fault <critical@fault.io>
2020-08-14 06:29:29 -0700

    Rely on project.txt for abstract and remove old factor type specification.

--- time/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-# Integer based datetime package with extensible unit definitions.
-# Includes system clock access, formatting and parsing of standard time
-# representations, infinity concepts, and time zone data file parsing.
-"""
-__factor_type__ = 'project'
