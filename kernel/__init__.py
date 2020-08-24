da56b50bf918a8caec414cbad936da2134614068
fault <critical@fault.io>
2020-08-24 08:27:06 -0700

    Remove old factor declaration.

--- kernel/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-# Hierarchically structured, asynchronous I/O framework for building applications and servers.
-
-# &..io is heavily modelled after UNIX's process structure. &.library.Sector instances represent
-# concepts similar to processes and &.library.Flow instances represent unidirectional file descriptors.
-"""
-__factor_type__ = 'project'
