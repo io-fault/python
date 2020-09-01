8811a215ce8d0e162377e99cb43d87d45cce2bd0
fault <critical@fault.io>
2020-09-01 14:18:32 -0700

    Correct sanity constraint.

--- terminal/test/test_matrix.py
+++ terminal/test/test_matrix.py
@@ -123,7 +123,7 @@ def test_Screen_methods(test):
 	b'2' in test/sr
 
 	b's' in test/S.store_cursor_location()
-	b'r' in test/S.restore_cursor_location()
+	b'u' in test/S.restore_cursor_location()
 	b'8' in test/S.scroll_up(8)
 	b'8' in test/S.scroll_down(8)
 
