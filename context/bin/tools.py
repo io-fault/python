cf66f0b25ca8080f894b6ed7deb82d4c4bd9bddb
fault <critical@fault.io>
2021-06-05 13:13:42 -0700

    Add tool index module.

---
+++ context/bin/tools.py
@@ -0,0 +1,20 @@
+"""
+# Executable factor index for &fault.system.bin.tool bindings.
+"""
+import importlib
+from ... import __name__ as context_name
+from ...system import process
+
+index = {
+	'python': context_name + '.system.bin.subexec',
+	'http-cache': context_name + '.web.bin.cache',
+	'test-python-module': context_name + '.test.bin.coherence',
+}
+
+def activate(factor, element, interface=None):
+	importlib.import_module(factor).extend(lambda x: (index[x], 'main'))
+
+def main(inv:process.Invocation) -> process.Exit:
+	import sys
+	for item in index.items():
+		sys.stdout.write("%s %s\n" % item)
