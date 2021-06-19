627660c2975f864b794777f564803ce95b2320a5
fault <critical@fault.io>
2021-06-19 13:25:30 -0700

    Eliminate project package module content and documentation.

--- context/__init__.py
+++ context/__init__.py
@@ -1,12 +1,1 @@
-"""
-# Fault Context Package
-
-# Provides bootstrapping tools for fault environments.
-
-# [Executables]
-
-# /&.bin.prepare
-	# Prepare the context package for use.
-"""
-__factor_type__ = 'project'
-context = None
+# Project package module for supporting sys.path imports.
