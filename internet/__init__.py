d3e7587b8ba8faf09902e466ec2adebc879414a2
fault <critical@fault.io>
2021-06-19 13:25:30 -0700

    Eliminate project package module content and documentation.

--- internet/__init__.py
+++ internet/__init__.py
@@ -1,9 +1,1 @@
-"""
-# internet is a project providing tools for working with DNS, HTTP, and SMTP.
-# It is implemented with event driven operation in mind, but does not
-# include any code for actually performing any communication; it merely structures
-# data read from a Wire and serializes data that is to be sent to the Wire. It
-# is intended to be a dependency of frameworks, applications, or servers that are
-# implementing support for HTTP or DNS.
-"""
-__factor_type__ = 'project'
+# Project package module for supporting sys.path imports.
