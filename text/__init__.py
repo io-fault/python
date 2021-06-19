7243d1cde4e44449c3181cd345729384eee6d5d8
fault <critical@fault.io>
2021-06-19 13:25:31 -0700

    Eliminate project package module content and documentation.

--- text/__init__.py
+++ text/__init__.py
@@ -1,13 +1,1 @@
-"""
-# [ Executables ]
-
-# /&.bin.xml
-	# Write structured XML to standard output using the fault.text from standard input.
-	# Most errors that occur will be reported using XML elements describing the failure.
-
-# /&.bin.tokenize
-	# Emit the tokens generated from the fault.text given to standard input to standard
-	# output. Generally only used by developers working on &..text in order to identify
-	# the constructed tokens from the input.
-"""
-__factor_type__ = 'project'
+# Project package module for supporting sys.path imports.
